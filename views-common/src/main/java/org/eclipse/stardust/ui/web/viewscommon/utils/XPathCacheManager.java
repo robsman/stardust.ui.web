8f45f823cafb459da4759015b42e98faf31b0595
Subodh Godbole <subodh.godbole@sungard.com>
2012-05-23 10:58:24 +0000

    Jira-ID: CRNT-24444 - Merge of XPathCacheManager from 5.3.x to 7.0 (Rev. 56103)
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ui.web@56511 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- views-common/src/main/java/org/eclipse/stardust/ui/web/viewscommon/utils/XPathCacheManager.java
+++ views-common/src/main/java/org/eclipse/stardust/ui/web/viewscommon/utils/XPathCacheManager.java
@@ -10,18 +10,28 @@
  *******************************************************************************/
 package org.eclipse.stardust.ui.web.viewscommon.utils;
 
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+import javax.xml.namespace.QName;
+
+import org.eclipse.stardust.common.config.Parameters;
 import org.eclipse.stardust.engine.api.model.Data;
 import org.eclipse.stardust.engine.api.model.DataPath;
 import org.eclipse.stardust.engine.api.model.Model;
-import org.eclipse.stardust.engine.api.model.Reference;
+import org.eclipse.stardust.engine.api.model.TypeDeclaration;
 import org.eclipse.stardust.engine.core.struct.ClientXPathMap;
 import org.eclipse.stardust.engine.core.struct.IXPathMap;
+import org.eclipse.stardust.engine.core.struct.StructuredDataConstants;
+import org.eclipse.stardust.engine.core.struct.StructuredTypeRtUtils;
+import org.eclipse.stardust.engine.extensions.dms.data.DmsConstants;
 import org.eclipse.stardust.ui.web.common.log.LogManager;
 import org.eclipse.stardust.ui.web.common.log.Logger;
 import org.eclipse.stardust.ui.web.common.util.FacesUtils;
+
+import com.infinity.bpm.thirdparty.xsd.XSDNamedComponent;
+import com.infinity.bpm.thirdparty.xsd.XSDSchema;
 
 
 
@@ -31,10 +41,26 @@ import org.eclipse.stardust.ui.web.common.util.FacesUtils;
  */
 public class XPathCacheManager
 {
+   public static final String PARAM_ENABLED = "Carnot.Client.Caching.ClientXPathMap.Enabled";
    public static final String BEAN_ID = "ippXPathCacheManager";
    public static final Logger trace = LogManager.getLogger(XPathCacheManager.class);
 
-   private Map<String, IXPathMap> xPathMapCache = new HashMap<String, IXPathMap>();
+   private boolean cacheEnabled = true;
+
+   // Key = TypeDeclarationCacheKey (QName + Model OID)
+   private Map<TypeDeclarationCacheKey, IXPathMap> typeXPathMapCache = new ConcurrentHashMap<TypeDeclarationCacheKey, IXPathMap>();
+   
+   // Key = model OID + element OID of the Data
+   private Map<Long, IXPathMap> dataXPathMapCache = new ConcurrentHashMap<Long, IXPathMap>();
+
+   /**
+    * 
+    */
+   public XPathCacheManager()
+   {
+      Parameters parameters = Parameters.instance();
+      cacheEnabled = parameters.getBoolean(PARAM_ENABLED, true);
+   }
 
    /**
     * @return
@@ -51,11 +77,14 @@ public class XPathCacheManager
    {
       try
       {
-         xPathMapCache.clear();
+         dataXPathMapCache.clear();
+         typeXPathMapCache.clear();
       }
       catch (Exception e)
       {
-         xPathMapCache = null;
+         dataXPathMapCache = null;
+         typeXPathMapCache = null;
+         
          trace.error("Error while resetting XPathCacheManager", e);
       }
    }
@@ -67,59 +96,201 @@ public class XPathCacheManager
     */
    public IXPathMap getXpathMap(Model model, DataPath dataPath)
    {
-      IXPathMap xPathMap = null;
+      Data data = model.getData(dataPath.getData());
 
-      String cachKey = getCacheKey(model, dataPath);
-      if (xPathMapCache.containsKey(cachKey))
+      if (!cacheEnabled)
       {
-         if (trace.isInfoEnabled())
+         if (trace.isDebugEnabled())
          {
-            trace.info("Reading XPathMap from cache for-> DataPath: " + dataPath.getId() + ", CacheKey: " + cachKey);
+            trace.debug("ClientXPathMap Caching is not enabled. Creating New and returning the same");
+         }
+         return ClientXPathMap.getXpathMap(model, data);
+      }
+
+      // Caching Mechanism
+      Long dataCachKey = getDataCacheKey(model, data);
+      IXPathMap xPathMap = dataXPathMapCache.get(dataCachKey);
+      if (null == xPathMap)
+      {
+         TypeDeclarationCacheKey typeCachKey = getTypeCacheKey(model, data);
+         xPathMap = typeXPathMapCache.get(typeCachKey);
+         if (null == xPathMap)
+         {
+            xPathMap = createIXPathMap(model, data, typeCachKey);
+            typeXPathMapCache.put(typeCachKey, xPathMap);
+            if (trace.isDebugEnabled())
+            {
+               trace.debug("Added XPathMap to typeXPathMapCache, Key = " + typeCachKey);
+            }
+         }
+         else
+         {
+            if (trace.isDebugEnabled())
+            {
+               trace.debug("Returning XPathMap from Type Cache, for typeCachKey = " + typeCachKey);
+            }
          }
 
-         xPathMap = xPathMapCache.get(cachKey);
+         // Put the xPathMap in Data Cache
+         dataXPathMapCache.put(dataCachKey, xPathMap);
       }
       else
-      {          
-         if (trace.isInfoEnabled())
+      {
+         if (trace.isDebugEnabled())
          {
-            trace.info("Computing XPathMap for-> DataPath: " + dataPath.getId() + ", CacheKey: " + cachKey);
+            trace.debug("Returning XPathMap from Data Cache, for dataCachKey = " + dataCachKey);
          }
-
-         Model refModel = getReferenceModel(model, dataPath);
-         xPathMap = ClientXPathMap.getXpathMap(refModel, model.getData(dataPath.getData()));
-         xPathMapCache.put(cachKey, xPathMap);
       }
-      
+
+      //printCacheKeys();
+
       return xPathMap;
    }
-   
+
    /**
     * @param model
-    * @param dataPath
+    * @param data
     * @return
     */
-   private String getCacheKey(Model model, DataPath dataPath)
+   private Long getDataCacheKey(Model model, Data data)
    {
-      return model.getModelOID() + ":" + dataPath.getElementOID();
+      long key = model.getModelOID() << 32 + data.getElementOID();
+      return key;
    }
 
    /**
     * @param model
-    * @param dp
+    * @param data
     * @return
     */
-   private Model getReferenceModel(Model model, DataPath dp)
+   private TypeDeclarationCacheKey getTypeCacheKey(Model model, Data data)
    {
-      Data data = model.getData(dp.getData());
-      Reference ref = data.getReference();
-      Model refModel = model;
+      TypeDeclaration typeDeclaration = null;
 
-      if (ref != null)
+      String dataTypeId = data.getTypeId();
+      if (dataTypeId.equals(StructuredDataConstants.STRUCTURED_DATA))
       {
-        refModel = ModelCache.findModelCache().getModel(ref.getModelOid());
+         // user-defined structured data
+         String typeDeclarationId = (String) data.getAttribute(StructuredDataConstants.TYPE_DECLARATION_ATT);
+         typeDeclaration = model.getTypeDeclaration(typeDeclarationId);
       }
-      
-      return refModel;
+      else
+      {
+         // build-in schema
+         String metadataComplexTypeName = (String)data.getAttribute(DmsConstants.RESOURCE_METADATA_SCHEMA_ATT);
+         typeDeclaration = model.getTypeDeclaration(metadataComplexTypeName);
+      }
+
+      XSDSchema schema = StructuredTypeRtUtils.getXSDSchema(model, typeDeclaration);
+      XSDNamedComponent component = StructuredTypeRtUtils.findElementOrTypeDeclaration(schema, typeDeclaration.getId(),
+            true);
+
+      QName qName = null;
+      if (null != component.getTargetNamespace())
+      {
+         qName = new QName(component.getTargetNamespace(), component.getName());
+      }
+      else
+      {
+         qName = new QName(component.getName());
+      }
+
+      return new TypeDeclarationCacheKey(qName, model.getModelOID());
+   }
+
+   /**
+    * @param model
+    * @param data
+    * @param typeCachKey
+    * @return
+    */
+   private IXPathMap createIXPathMap(Model model, Data data, TypeDeclarationCacheKey typeCachKey)
+   {
+      IXPathMap xPathMap = ClientXPathMap.getXpathMap(model, data);
+      if (trace.isDebugEnabled())
+      {
+         trace.debug("Created new XPathMap for typeCachKey = " + typeCachKey);
+      }
+
+      // TODO: To compare the contents of newly created IXPathMap with typeXPathMapCache.
+      // Because for internal structured data that have been changed in 2 model versions,
+      // they will still have the same QName, but different content.
+      // Compare the content of the maps, and only if the content are equal then ignore
+      // the newly created map and use the one already existing in typeXPathMapCache.
+
+      return xPathMap;
+   }
+
+   /**
+    * 
+    */
+   private void printCacheKeys()
+   {
+      if (trace.isDebugEnabled())
+      {
+         trace.debug("Data Cache Keys =");
+         Iterator<Long> it1 = dataXPathMapCache.keySet().iterator();
+         while (it1.hasNext())
+         {
+            trace.debug("\t[DataCache]:" + it1.next());
+         }
+         
+         trace.debug("Type Cache Keys =");
+         Iterator<TypeDeclarationCacheKey> it2 = typeXPathMapCache.keySet().iterator();
+         while (it2.hasNext())
+         {
+            trace.debug("\t[TypeCache]:" + it2.next());
+         }
+      }
+   }
+
+   /**
+    * @author florin.herinean
+    *
+    */
+   private static class TypeDeclarationCacheKey
+   {
+      private QName qname;
+      private int modelOID;
+
+      /**
+       * @param qname
+       * @param modelOID
+       */
+      public TypeDeclarationCacheKey(QName qname, int modelOID)
+      {
+         this.qname = qname;
+         this.modelOID = modelOID;
+      }
+
+      @Override
+      public int hashCode()
+      {
+         return 961 + 31 * modelOID + qname.hashCode();
+      }
+
+      @Override
+      public boolean equals(Object obj)
+      {
+         return this == obj || obj instanceof TypeDeclarationCacheKey
+               && modelOID == ((TypeDeclarationCacheKey) obj).modelOID
+               && qname.equals(((TypeDeclarationCacheKey) obj).qname);
+      }
+ 
+      @Override
+      public String toString()
+      {
+         return qname + ":" + modelOID;
+      }
+
+      public QName getQname()
+      {
+         return qname;
+      }
+
+      public int getModelOID()
+      {
+         return modelOID;
+      }
    }
 }
