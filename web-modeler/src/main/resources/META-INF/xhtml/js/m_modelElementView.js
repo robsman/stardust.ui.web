c505cb696abf89b243ed65d7bddb39b5b5b518c8
Marc Gille <marc.gille@sungard.com>
2012-07-30 07:08:11 +0000

    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ui.web@58018 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

---
+++ web-modeler/src/main/resources/META-INF/xhtml/js/m_modelElementView.js
@@ -0,0 +1,97 @@
+/**
+ * @author Marc.Gille
+ */
+define(
+		[ "m_utils", "m_constants", "m_command", "m_commandsController", "m_dialog", "m_view" ],
+		function(m_utils, m_constants, m_command, m_commandsController, m_dialog, m_view) {
+			return {
+				create : function(id) {
+					var view = new ModelElementView();
+
+					return view;
+				}
+			};
+
+			/**
+			 * 
+			 */
+			function ModelElementView() {
+				// Inheritance
+
+				var view = m_view.create();
+
+				m_utils.inheritFields(this, view);
+				m_utils.inheritMethods(ModelElementView.prototype, view);
+
+				this.modelElement = null;
+				this.guidOutput = jQuery("#guidOutput");
+				this.idOutput = jQuery("#idOutput");
+				this.nameInput = jQuery("#nameInput");
+				this.descriptionTextarea = jQuery("#descriptionTextarea");
+
+				this.nameInput.change({
+					"view" : this
+				}, function(event) {
+					var view = event.data.view;
+
+					if (!view.validate()) {
+						return;
+					}
+
+					if (view.application.name != view.nameInput.val()) {
+						view.submitChanges({
+							name : view.nameInput.val()
+						});
+					}
+				});
+				this.descriptionTextarea.change({
+					"view" : this
+				}, function(event) {
+					var view = event.data.view;
+
+					if (!view.validate()) {
+						return;
+					}
+
+					if (view.application.description != view.descriptionTextarea.val()) {
+						view.submitChanges({
+							description : view.descriptionTextarea.val()
+						});
+					}
+				});
+
+				/**
+				 * 
+				 */
+				ModelElementView.prototype.initializeModelElement = function(
+						modelElement) {
+					this.modelElement = modelElement;
+					this.guidOutput.empty();
+					this.guidOutput.append(this.modelElement.oid);
+					this.idOutput.empty();
+					this.idOutput.append(this.modelElement.id);
+					this.nameInput.val(this.modelElement.name);
+					this.descriptionTextarea.val(this.modelElement.description);
+
+					if (this.modelElement.attributes == null) {
+						this.modelElement.attributes = {};
+					}
+				}
+
+				/**
+				 * 
+				 */
+				ModelElementView.prototype.submitChanges = function(changes) {
+					// Generic attributes
+
+					if (changes.attributes == null) {
+						changes.attributes = {};
+					}
+
+					m_commandsController.submitCommand(m_command
+							.createUpdateModelElementCommand(
+									this.modelElement.model.id,
+									this.modelElement.oid, changes));
+				};
+			}
+		});
