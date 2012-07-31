9b7cfa457ea61e512b223f8c0275eef0584fb61d
Robert Sauer <robert.sauer@sungard.com>
2012-07-31 09:27:03 +0000

    Introducing extension contract for command handlers.
    Adding JavaDoc for the annotations.
    
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ui.web@58044 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- web-modeler/src/main/java/org/eclipse/stardust/ui/web/modeler/edit/spi/CommandHandler.java
+++ web-modeler/src/main/java/org/eclipse/stardust/ui/web/modeler/edit/spi/CommandHandler.java
@@ -6,6 +6,14 @@ import java.lang.annotation.RetentionPolicy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+/**
+ * Declares a command handling Spring bean. Implies "singleton" scope. Actual handler methods must be annotated with {@link OnCommand}.
+ * <p>
+ * This annotation must be used to enable automatic discovery of command handlers.
+ *
+ * @author robert.sauer
+ * @see @OnCommand
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Component
 @Scope("singleton")
--- web-modeler/src/main/java/org/eclipse/stardust/ui/web/modeler/edit/spi/OnCommand.java
+++ web-modeler/src/main/java/org/eclipse/stardust/ui/web/modeler/edit/spi/OnCommand.java
@@ -3,6 +3,32 @@ package org.eclipse.stardust.ui.web.modeler.edit.spi;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+import com.google.gson.JsonObject;
+
+/**
+ * Declares a command handler method. Handler methods must accept two parameters
+ * <ol>
+ * <li> the target element
+ * <li> the {@link JsonObject} representation of command arguments
+ * </ol>
+ * <p>
+ * Example:
+ * <pre>
+ * {@code
+ * &#64;CommandHandler
+ * public class MoveNodeSymbolHandler
+ * {
+ *    &#64;OnCommand(commandId = "nodeSymbol.move")
+ *    public void handleMoveNode(INodeSymbol nodeSymbol, JsonObject request)
+ *    {
+ *       ...
+ *    }
+ * }
+ * </pre>
+ *
+ * @author robert.sauer
+ * @see @CommandHandler
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface OnCommand
 {
