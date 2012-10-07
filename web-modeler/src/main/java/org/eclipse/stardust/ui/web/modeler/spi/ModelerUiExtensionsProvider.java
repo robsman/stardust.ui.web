fd93493c3c52cbcdfe2b8448385fdb91484d2749
Marc Gille <marc.gille@sungard.com>
2012-10-07 09:45:51 +0000

    Addressed CRNT-26312.
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ui.web@59786 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

---
+++ web-modeler/src/main/java/org/eclipse/stardust/ui/web/modeler/spi/ModelerUiExtensionsProvider.java
@@ -0,0 +1,175 @@
+package org.eclipse.stardust.ui.web.modeler.spi;
+
+import java.util.List;
+
+public interface ModelerUiExtensionsProvider
+{
+   List<ViewManagerConfiguration> getViewManagerConfigs();
+
+   List<PropertyPageConfiguration> getPropertyPageConfigs();
+
+   List<ToolbarSectionConfiguration> getToolbarSectionConfigs();
+
+   List<ToolbarEntryConfiguration> getToolbarEntryConfigs();
+
+   List<ApplicationTypeConfiguration> getApplicationTypeConfigs();
+
+   List<DataTypeConfiguration> getDataTypeConfigs();
+
+   class ViewManagerConfiguration
+   {
+      public final String id;
+
+      public final String moduleUri;
+
+      public ViewManagerConfiguration(String id, String moduleUri)
+      {
+         this.id = id;
+         this.moduleUri = moduleUri;
+      }
+   }
+
+   class PropertyPageConfiguration
+   {
+      public final String pageId;
+
+      public final String panelId;
+
+      public final String pageUri;
+
+      public final String moduleUri;
+
+      public final String visibility;
+
+      public final String pageName;
+
+      public final String iconPath;
+
+      public PropertyPageConfiguration(String panelId, String pageId, String moduleUri, String visibility)
+      {
+         this.pageId = pageId;
+         this.panelId = panelId;
+         this.pageUri = null;
+         this.moduleUri = moduleUri;
+         this.visibility = visibility;
+         this.pageName = null;
+         this.iconPath = null;
+      }
+
+      public PropertyPageConfiguration(String panelId, String pageId, String pageUri, String moduleUri, String visibility)
+      {
+         this.pageId = pageId;
+         this.panelId = panelId;
+         this.pageUri = pageUri;
+         this.moduleUri = moduleUri;
+         this.visibility = visibility;
+         this.pageName = null;
+         this.iconPath = null;
+      }
+
+      public PropertyPageConfiguration(String panelId, String pageId, String pageUri, String moduleUri, String visibility, String pageName, String iconPath)
+      {
+         this.pageId = pageId;
+         this.panelId = panelId;
+         this.pageUri = pageUri;
+         this.moduleUri = moduleUri;
+         this.visibility = visibility;
+         this.pageName = pageName;
+         this.iconPath = iconPath;
+      }
+   }
+
+   class ToolbarSectionConfiguration
+   {
+      public final String id;
+
+      public final String title;
+
+      public final String pageUri;
+
+      public final String moduleUri;
+
+      public final String visibility;
+
+      public ToolbarSectionConfiguration(String id, String title, String visibility)
+      {
+         this.id = id;
+         this.title = title;
+         this.pageUri = null;
+         this.moduleUri = null;
+         this.visibility = visibility;
+      }
+
+      public ToolbarSectionConfiguration(String id, String title, String pageUri, String moduleUri, String visibility)
+      {
+         this.id = id;
+         this.title = title;
+         this.pageUri = pageUri;
+         this.moduleUri = moduleUri;
+         this.visibility = visibility;
+      }
+   }
+
+   class ToolbarEntryConfiguration
+   {
+      public final String id;
+
+      public final String sectionId;
+
+      public final String title;
+
+      public final String iconUri;
+
+      public final String moduleUri;
+
+      public final String handlerMethod;
+
+      public final String visibility;
+
+      public ToolbarEntryConfiguration(String id, String sectionId, String title, String iconUri, String moduleUri, String handlerMethod, String visibility)
+      {
+         this.id = id;
+         this.sectionId = sectionId;
+         this.title = title;
+         this.iconUri = iconUri;
+         this.moduleUri = moduleUri;
+         this.handlerMethod = handlerMethod;
+         this.visibility = visibility;
+      }
+   }
+
+   class ApplicationTypeConfiguration
+   {
+      public final String id;
+
+      public final String title;
+
+      public final String iconUri;
+
+      public final String viewId;
+
+      public ApplicationTypeConfiguration(String id, String title, String iconUri, String viewId)
+      {
+         this.id = id;
+         this.title = title;
+         this.iconUri = iconUri;
+         this.viewId = viewId;
+      }
+   }
+
+   class DataTypeConfiguration
+   {
+      public final String id;
+
+      public final String title;
+
+      public final String iconUri;
+
+      public DataTypeConfiguration(String id, String title, String iconUri)
+      {
+         this.id = id;
+         this.title = title;
+         this.iconUri = iconUri;
+      }
+   }
+}
