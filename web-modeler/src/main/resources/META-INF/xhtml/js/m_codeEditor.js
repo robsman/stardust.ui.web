7fa163561dc2ed70973b603a6bc30c675f497c63
Anoop Nair <anoop.nair@sungard.com>
2012-10-30 21:50:04 +0000

    Jira-ID: CRNT-26351 - Enabled JS validation - Code clean-up and some re-factoring
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ui.web@60422 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- web-modeler/src/main/resources/META-INF/xhtml/js/m_codeEditor.js
+++ web-modeler/src/main/resources/META-INF/xhtml/js/m_codeEditor.js
@@ -10,88 +10,38 @@
  ******************************************************************************/
 
 define(
-		[ "jquery" ],
-		function() {
+		[ "jquery", "jshint" ],
+		function(jquery, jshint) {
+			// Interface
 			return {
 				getCodeEditor : function(textArea) {
 					return new CodeEditor(textArea);
 				}
 			};
 		
-			function getErrors(source) {
-				var errors = {};
-				/*var err, lineNumber;
-				
-				var options = {undef: true, smarttabs: true};
-				var globalData = m_dataTraversal.getAllDataAsJavaScriptObjects(m_model.findModel('Loan'));
-				var globals = {};
-				for (var variable in globalData) {
-					globals[variable] = true;
-				}
-				JSHINT(source, options, globals);
-				for (var i = 0; i < JSHINT.errors.length; ++i) {
-					err = JSHINT.errors[i];
-					if (!err) continue;
-					lineNumber = err.line - 1;
-					if (!errors[lineNumber]) errors[lineNumber] = []; 
-					errors[lineNumber].push(err.reason);
-				}*/
-				
-				return errors;
-			}
-
-			var errorLineNumbers = [];
-			function showErrors() {
-				/*editor.operation(function(){
-					for (var i = 0; i < errorLineNumbers.length; ++i)
-						editor.clearMarker(errorLineNumbers[i]);
-					errorLineNumbers.length = 0;
-				
-					var html;
-					var errors = getErrors(editor.getValue());
-					for (var lineNumber in errors) {
-						html = '<div class="gutter-warning"><div class="tooltip"><ul>';
-						for (var i = 0; i < errors[lineNumber].length; i++)
-							html += '<li>' + errors[lineNumber][i] + '</li>';
-						html += '</ul></div> %N%</div>';
-						
-						errorLineNumbers.push(Number(lineNumber));
-						editor.setMarker(Number(lineNumber), html);
-					}
-				});*/
-				
-				jQuery('.gutter-warning').hover(function() {
-					// Hover over code
-					var title = jQuery(this).attr('title');
-					jQuery(this).data('tipText', title).removeAttr('title');
-
-					var tooltip = jQuery(this).children('.tooltip :first'); 
-					// tooltip.position({at: 'right top', of: jQuery(this), my: 'right top'});
-					tooltip.css({ marginLeft: '+=45px', marginTop: '+=15px' });
-					tooltip.fadeIn('slow');
-				}, function() {
-						// Hover out code
-						jQuery(this).attr('title', jQuery(this).data('tipText'));
-						var tooltip = jQuery(this).children('.tooltip :first');
-						tooltip.hide();
-						tooltip.css({ marginLeft: '-=45px', marginTop: '-=15px' });
-				});
-			
-				var info = editor.getScrollInfo();
-				var after = editor.charCoords({line: editor.getCursor().line + 1, ch: 0}, "local").top;
-				if (info.top + info.clientHeight < after)
-					editor.scrollTo(null, after - info.clientHeight + 3);
-			}
-
 			function CodeEditor(textArea) {
 		
+				var editor = null;
+				var wrapper = null;
+				
+				var globalData = null;
+				var hlLine = null;
+				
+				var EDITOR_STYLECLASS_ENABLED = "CodeMirror-enabled";
+				var EDITOR_STYLECLASS_DISABLED = "CodeMirror-disabled";
+				var EDITOR_STYLECLASS_MATCH_HIGHLIGHT = "CodeMirror-matchhighlight";
+				var EDITOR_STYLECLASS_ACTIVELINE = "activeline";
+				
+				var EDITOR_READONLY = "readOnly";
+				var EDITOR_NOCURSOR = "nocursor";
+				
 				// Set up code editor for JS code expression
 				CodeMirror.commands.autocomplete = function(cm) {
 					CodeMirror.simpleHint(cm, CodeMirror.javascriptHint);
 				};
 
 				var waiting;
-				var editor = CodeMirror.fromTextArea(textArea, {
+				editor = CodeMirror.fromTextArea(textArea, {
 					mode: "javascript",
 					theme: "eclipse",
 					lineNumbers: true,
@@ -101,10 +51,10 @@ define(
 					extraKeys: {"Ctrl-Space": "autocomplete"},
 					onCursorActivity: function() {
 						// Highlight selected text
-						editor.matchHighlight("CodeMirror-matchhighlight");
+						editor.matchHighlight(EDITOR_STYLECLASS_MATCH_HIGHLIGHT);
 						// Set active line
 						editor.setLineClass(hlLine, null, null);
-						hlLine = editor.setLineClass(editor.getCursor().line, null, "activeline");
+						hlLine = editor.setLineClass(editor.getCursor().line, null, EDITOR_STYLECLASS_ACTIVELINE);
 					},
 					onBlur: function() {
 						editor.save();
@@ -117,27 +67,88 @@ define(
 						waiting = setTimeout(showErrors, 500);
 					}
 				});
-				var hlLine = editor.setLineClass(0, "activeline");
+				
+				wrapper = editor.getWrapperElement();
+				hlLine = editor.setLineClass(0, EDITOR_STYLECLASS_ACTIVELINE);
 				setTimeout(showErrors, 100);
 				
-				var wrapper = editor.getWrapperElement();
 		
+				function getErrors(source) {
+					var errors = {};
+					var err, lineNumber;
+					
+					var options = {undef: true, smarttabs: true};
+					var globals = {};
+					for (var variable in globalData) {
+						globals[variable] = true;
+					}
+					JSHINT(source, options, globals);
+					for (var i = 0; i < JSHINT.errors.length; ++i) {
+						err = JSHINT.errors[i];
+						if (!err) continue;
+						lineNumber = err.line - 1;
+						if (!errors[lineNumber]) errors[lineNumber] = []; 
+						errors[lineNumber].push(err.reason);
+					}
+					
+					return errors;
+				}
+
+				function showErrors() {
+					var errorLineNumbers = [];
+					editor.operation(function(){
+						for (var i = 0; i < errorLineNumbers.length; ++i)
+							editor.clearMarker(errorLineNumbers[i]);
+						errorLineNumbers.length = 0;
+					
+						var html;
+						var errors = getErrors(editor.getValue());
+						for (var lineNumber in errors) {
+							html = '<div class="gutter-warning"><div class="tooltip"><ul>';
+							for (var i = 0; i < errors[lineNumber].length; i++)
+								html += '<li>' + errors[lineNumber][i] + '</li>';
+							html += '</ul></div> %N%</div>';
+							
+							errorLineNumbers.push(Number(lineNumber));
+							editor.setMarker(Number(lineNumber), html);
+						}
+					});
+					
+					jQuery('.gutter-warning').hover(function() {
+						// Hover over code
+						var title = jQuery(this).attr('title');
+						jQuery(this).data('tipText', title).removeAttr('title');
+
+						var tooltip = jQuery(this).children('.tooltip :first'); 
+						// tooltip.position({at: 'right top', of: jQuery(this), my: 'right top'});
+						tooltip.css({ marginLeft: '+=45px', marginTop: '+=15px' });
+						tooltip.fadeIn('slow');
+					}, function() {
+							// Hover out code
+							jQuery(this).attr('title', jQuery(this).data('tipText'));
+							var tooltip = jQuery(this).children('.tooltip :first');
+							tooltip.hide();
+							tooltip.css({ marginLeft: '-=45px', marginTop: '-=15px' });
+					});
+				
+					var info = editor.getScrollInfo();
+					var after = editor.charCoords({line: editor.getCursor().line + 1, ch: 0}, "local").top;
+					if (info.top + info.clientHeight < after)
+						editor.scrollTo(null, after - info.clientHeight + 3);
+				}
+
 				CodeEditor.prototype.setValue = function(val) {
 					editor.setValue(val);
 				};
 		
-				/*CodeEditor.prototype.setOption = function(id, value) {
-					editor.setOption(id, value);
-				};*/
-		
 				CodeEditor.prototype.disable = function() {
-					editor.setOption("readOnly", "nocursor");
-					jQuery(wrapper).addClass("CodeMirror-disabled");
+					editor.setOption(EDITOR_READONLY, EDITOR_NOCURSOR);
+					jQuery(wrapper).addClass(EDITOR_STYLECLASS_ENABLED);
 				};
 
 				CodeEditor.prototype.enable = function() {
-					editor.setOption("readOnly", false);
-					jQuery(wrapper).removeClass("CodeMirror-disabled");
+					editor.setOption(EDITOR_READONLY, false);
+					jQuery(wrapper).removeClass(EDITOR_STYLECLASS_DISABLED);
 				};
 		
 				CodeEditor.prototype.getWrapper = function() {
@@ -145,9 +156,7 @@ define(
 				};
 		
 				CodeEditor.prototype.setGlobalData = function(globalVariables) {
-					for (var key in globalVariables) {
-						window[key] = globalVariables[key];
-					}
+					globalData = globalVariables;
 				};
 		
 				CodeEditor.prototype.save = function() {
