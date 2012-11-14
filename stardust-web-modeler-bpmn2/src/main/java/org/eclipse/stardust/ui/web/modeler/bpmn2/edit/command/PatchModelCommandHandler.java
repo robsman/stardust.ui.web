035b248963ae4386277cd294db5eeff73d92bc3d
Robert Sauer <robert.sauer@sungard.com>
2012-11-14 09:59:33 +0000

    Diagram elements have a different base type than semantic elements, extending command handler accordingly.
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ui.web@60818 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-web-modeler-bpmn2/src/main/java/org/eclipse/stardust/ui/web/modeler/bpmn2/edit/command/PatchModelCommandHandler.java
+++ stardust-web-modeler-bpmn2/src/main/java/org/eclipse/stardust/ui/web/modeler/bpmn2/edit/command/PatchModelCommandHandler.java
@@ -1,18 +1,14 @@
 package org.eclipse.stardust.ui.web.modeler.bpmn2.edit.command;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.eclipse.bpmn2.BaseElement;
 import org.eclipse.bpmn2.Definitions;
+import org.eclipse.dd.di.DiagramElement;
 
 import com.google.gson.JsonObject;
 
 import org.eclipse.stardust.common.log.LogManager;
 import org.eclipse.stardust.common.log.Logger;
-import org.eclipse.stardust.model.xpdl.builder.utils.ModelerConstants;
 import org.eclipse.stardust.ui.web.modeler.bpmn2.Bpmn2ModelUnmarshaller;
-import org.eclipse.stardust.ui.web.modeler.bpmn2.utils.Bpmn2ExtensionUtils;
 import org.eclipse.stardust.ui.web.modeler.edit.spi.CommandHandler;
 import org.eclipse.stardust.ui.web.modeler.edit.spi.OnCommand;
 
@@ -20,12 +16,12 @@ import org.eclipse.stardust.ui.web.modeler.edit.spi.OnCommand;
 public class PatchModelCommandHandler
 {
    private static final Logger trace = LogManager.getLogger(PatchModelCommandHandler.class);
-   private Bpmn2ModelUnmarshaller unmarshaller;
 
-   
+   private final Bpmn2ModelUnmarshaller unmarshaller;
+
    public PatchModelCommandHandler()
    {
-      unmarshaller = new Bpmn2ModelUnmarshaller();
+      this.unmarshaller = new Bpmn2ModelUnmarshaller();
    }
 
    @OnCommand(commandId = "modelElement.update")
@@ -33,6 +29,14 @@ public class PatchModelCommandHandler
          JsonObject patch)
    {
       trace.info("About to patch model element " + targetElement);
+
+      unmarshaller.populateFromJson(targetElement, patch);
+   }
+
+   @OnCommand(commandId = "modelElement.update")
+   public void patchBpmn2ModelElement(Definitions model, DiagramElement targetElement, JsonObject patch)
+   {
+      trace.info("About to patch diagram element " + targetElement);
 
       unmarshaller.populateFromJson(targetElement, patch);
    }
