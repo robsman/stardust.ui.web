475ec537bec05e5b6c8bb01b77cd50175ce6b692
Marc Gille <marc.gille@sungard.com>
2012-11-22 12:19:59 +0000

    Proper absolute pathes.
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ui.web@61031 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- web-modeler/src/main/resources/META-INF/xhtml/js/modelerExtensions/view/views.js
+++ web-modeler/src/main/resources/META-INF/xhtml/js/modelerExtensions/view/views.js
@@ -3,59 +3,59 @@ define([], function() {
 		view : [ {
 			viewId : "modelView",
 			viewLabel : "Model",
-			viewHtmlUrl : "../views/modeler/modelView.html"
+			viewHtmlUrl : "bpm-modeler/views/modeler/modelView.html"
 		}, {
 			viewId : "processDefinitionView",
 			viewLabel : "Process Definition",
-			viewHtmlUrl : "../views/modeler/processDefinitionView.html"
+			viewHtmlUrl : "bpm-modeler/views/modeler/processDefinitionView.html"
 		}, {
 			viewId : "roleView",
 			viewLabel : "Role",
-			viewHtmlUrl : "../views/modeler/roleView.html"
+			viewHtmlUrl : "bpm-modeler/views/modeler/roleView.html"
 		}, {
 			viewId : "organizationView",
 			viewLabel : "Organization",
-			viewHtmlUrl : "../views/modeler/organizationView.html"
+			viewHtmlUrl : "bpm-modeler/views/modeler/organizationView.html"
 		},
 		{
 			viewId : "conditionalPerformerView",
 			viewLabel : "Conditional Performer",
-			viewHtmlUrl : "../views/modeler/conditionalPerformerView.html"
+			viewHtmlUrl : "bpm-modeler/views/modeler/conditionalPerformerView.html"
 		},
 		{
 			viewId : "dataView",
 			viewLabel : "Data",
-			viewHtmlUrl : "../views/modeler/dataView.html"
+			viewHtmlUrl : "bpm-modeler/views/modeler/dataView.html"
 		},
 		{
 			viewId : "genericApplicationView",
 			viewLabel : "Unsupported Application",
-			viewHtmlUrl : "../views/modeler/genericApplicationView.html"
+			viewHtmlUrl : "bpm-modeler/views/modeler/genericApplicationView.html"
 		},
 		{
 			viewId : "webServiceApplicationView",
 			viewLabel : "Web Service",
-			viewHtmlUrl : "../views/modeler/webServiceApplicationView.html"
+			viewHtmlUrl : "bpm-modeler/views/modeler/webServiceApplicationView.html"
 		},
 		{
 			viewId : "uiMashupApplicationView",
 			viewLabel : "UI Mashup",
-			viewHtmlUrl : "../views/modeler/uiMashupApplicationView.html"
+			viewHtmlUrl : "bpm-modeler/views/modeler/uiMashupApplicationView.html"
 		},
 		{
 			viewId : "camelApplicationView",
 			viewLabel : "Camel Route",
-			viewHtmlUrl : "../views/modeler/camelApplicationView.html"
+			viewHtmlUrl : "bpm-modeler/views/modeler/camelApplicationView.html"
 		},
 		{
 			viewId : "messageTransformationApplicationView",
 			viewLabel : "Message Transformation",
-			viewHtmlUrl : "../views/modeler/messageTransformationApplicationView.html"
+			viewHtmlUrl : "bpm-modeler/views/modeler/messageTransformationApplicationView.html"
 		},
 		{
 			viewId : "xsdStructuredDataTypeView",
 			viewLabel : "Structured Data",
-			viewHtmlUrl : "../views/modeler/xsdStructuredDataTypeView.html"
+			viewHtmlUrl : "bpm-modeler/views/modeler/xsdStructuredDataTypeView.html"
 		}  ]
 	};
 });
