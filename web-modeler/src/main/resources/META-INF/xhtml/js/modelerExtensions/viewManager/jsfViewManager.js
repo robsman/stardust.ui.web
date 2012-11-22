fa70c3d6a919dd4a777f130e4e74a4945e00289d
Marc Gille <marc.gille@sungard.com>
2012-11-22 06:08:51 +0000

    require mechanism generalized.
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ui.web@61003 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- web-modeler/src/main/resources/META-INF/xhtml/js/modelerExtensions/viewManager/jsfViewManager.js
+++ web-modeler/src/main/resources/META-INF/xhtml/js/modelerExtensions/viewManager/jsfViewManager.js
@@ -1,8 +1,8 @@
-define([ 'm_jsfViewManager' ], function(m_jsfViewManager) {
+define([ 'bpm-modeler/js/m_jsfViewManager' ], function(m_jsfViewManager) {
 	return {
 		viewManager: {
 			id : "jsfViewManager",
-			provider : m_jsfViewManager,
-		},
+			provider : m_jsfViewManager
+		}
 	};
 });
