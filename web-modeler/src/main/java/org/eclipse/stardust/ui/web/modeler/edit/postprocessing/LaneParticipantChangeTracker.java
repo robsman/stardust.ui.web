205567c183b0c4738983df9b8ab2107ab89d24b6
Robert Sauer <robert.sauer@sungard.com>
2012-11-27 15:55:02 +0000

    Associating participant with manual trigger, too, if lane participant changes (CRNT-26011).
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ui.web@61185 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- web-modeler/src/main/java/org/eclipse/stardust/ui/web/modeler/edit/postprocessing/LaneParticipantChangeTracker.java
+++ web-modeler/src/main/java/org/eclipse/stardust/ui/web/modeler/edit/postprocessing/LaneParticipantChangeTracker.java
@@ -3,15 +3,21 @@ package org.eclipse.stardust.ui.web.modeler.edit.postprocessing;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
+import org.springframework.stereotype.Component;
+
+import org.eclipse.stardust.common.CompareHelper;
+import org.eclipse.stardust.engine.api.model.PredefinedConstants;
 import org.eclipse.stardust.model.xpdl.builder.session.Modification;
 import org.eclipse.stardust.model.xpdl.carnot.ActivitySymbolType;
 import org.eclipse.stardust.model.xpdl.carnot.ActivityType;
 import org.eclipse.stardust.model.xpdl.carnot.CarnotWorkflowModelPackage;
 import org.eclipse.stardust.model.xpdl.carnot.IModelParticipant;
 import org.eclipse.stardust.model.xpdl.carnot.LaneSymbol;
+import org.eclipse.stardust.model.xpdl.carnot.StartEventSymbol;
+import org.eclipse.stardust.model.xpdl.carnot.TriggerType;
 import org.eclipse.stardust.model.xpdl.carnot.util.ActivityUtil;
+import org.eclipse.stardust.model.xpdl.carnot.util.AttributeUtil;
 import org.eclipse.stardust.ui.web.modeler.edit.spi.ChangePostprocessor;
-import org.springframework.stereotype.Component;
 
 @Component
 public class LaneParticipantChangeTracker implements ChangePostprocessor
@@ -33,8 +39,7 @@ public class LaneParticipantChangeTracker implements ChangePostprocessor
       for (EObject candidate : change.getModifiedElements())
       {
          if ((candidate instanceof LaneSymbol)
-               && change
-                     .wasModified(candidate, PKG_XPDL.getISwimlaneSymbol_Participant()))
+               && change.wasModified(candidate, PKG_XPDL.getISwimlaneSymbol_Participant()))
          {
             LaneSymbol lane = (LaneSymbol) candidate;
             IModelParticipant participant = lane.getParticipant();
@@ -46,25 +51,35 @@ public class LaneParticipantChangeTracker implements ChangePostprocessor
    private void setParticipant(LaneSymbol lane, IModelParticipant participant)
    {
       List<ActivitySymbolType> activitySymbols = lane.getActivitySymbol();
-      for (int i = 0; i < activitySymbols.size(); i++)
+      for (int i = 0; i < activitySymbols.size(); i++ )
       {
          ActivitySymbolType activitySymbol = (ActivitySymbolType) activitySymbols.get(i);
          ActivityType activity = (ActivityType) activitySymbol.getModelElement();
 
          if (ActivityUtil.isInteractive(activity))
          {
-            setPerformer(activity, participant, activity.getPerformer(), false);
+            setPerformer(activity, participant, activity.getPerformer());
+         }
+      }
+
+      List<StartEventSymbol> startEventSymbols = lane.getStartEventSymbols();
+      for (int i = 0; i < startEventSymbols.size(); i++ )
+      {
+         StartEventSymbol startEventSymbol = startEventSymbols.get(i);
+         TriggerType trigger = startEventSymbol.getTrigger();
+
+         if ((null != trigger)
+               && PredefinedConstants.MANUAL_TRIGGER.equals(trigger.getType().getId()))
+         {
+            setPerformer(trigger, participant);
          }
       }
    }
 
-
-
-
    private void setPerformer(ActivityType activity, IModelParticipant newPerformer,
-         IModelParticipant originalPerformer, boolean updateConnectionsOnly)
+         IModelParticipant originalPerformer)
    {
-      if (!updateConnectionsOnly && newPerformer != originalPerformer)
+      if (newPerformer != originalPerformer)
       {
          activity.setPerformer(newPerformer);
          modification.markAlsoModified(activity);
@@ -72,12 +87,16 @@ public class LaneParticipantChangeTracker implements ChangePostprocessor
 
    }
 
-   public static String getPerformerName(IModelParticipant performer)
+   private void setPerformer(TriggerType manualTrigger, IModelParticipant newPerformer)
    {
-      return performer == null ? "" : performer.getName() != null //$NON-NLS-1$
-            ? performer.getName()
-            : performer.getId() != null ? performer.getId() : Long.toString(performer
-                  .getElementOid());
+      String originalPerformerId = AttributeUtil.getAttributeValue(manualTrigger,
+            PredefinedConstants.MANUAL_TRIGGER_PARTICIPANT_ATT);
+      String newPerformerId = (null != newPerformer) ? newPerformer.getId() : null;
+      if ( !CompareHelper.areEqual(newPerformerId, originalPerformerId))
+      {
+         AttributeUtil.setAttribute(manualTrigger,
+               PredefinedConstants.MANUAL_TRIGGER_PARTICIPANT_ATT, newPerformerId);
+         modification.markAlsoModified(manualTrigger);
+      }
    }
-
 }
