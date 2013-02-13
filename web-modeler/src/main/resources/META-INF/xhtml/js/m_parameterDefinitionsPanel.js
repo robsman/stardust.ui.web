dbbbc8096c123f5ab992ecd355b913920562df54
Shrikant Gangal <shrikant.gangal@sungard.com>
2013-02-13 12:26:17 +0000

    Jira-ID: CRNT-27886 Reverting changes as there are bigger problems with parmeter definition panel that needs fixing first
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ui.web@62973 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- web-modeler/src/main/resources/META-INF/xhtml/js/m_parameterDefinitionsPanel.js
+++ web-modeler/src/main/resources/META-INF/xhtml/js/m_parameterDefinitionsPanel.js
@@ -47,10 +47,8 @@ define(
 
 					if (this.options.scope == null) {
 						this.options.scope = "";
-						this.dataTypeSelectorScope = "parameterDefinitionTypeSelector";
 					} else {
 						this.options.scope = "#" + this.options.scope + " ";
-						this.dataTypeSelectorScope = this.options.scope;
 					}
 
 					// TODO: Change width via CSS and classes here
@@ -102,7 +100,7 @@ define(
 
 					if (this.options.supportsDataTypeSelection) {
 						this.dataTypeSelector = m_dataTypeSelector.create({
-							scope : this.dataTypeSelectorScope,
+							scope : "parameterDefinitionTypeSelector",
 							submitHandler : this,
 							supportsOtherData : false,
 							supportsDocumentTypes : true,
