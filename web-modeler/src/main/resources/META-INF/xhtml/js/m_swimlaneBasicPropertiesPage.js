8ac140d64e3e0977fe475930a9487d4406aeb4c4
Yogesh Manware <yogesh.manware@sungard.com>
2013-02-22 07:41:34 +0000

    Jira-ID: CRNT-28051 - corrected the comparison check of event symbol
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ui.web@63176 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- web-modeler/src/main/resources/META-INF/xhtml/js/m_swimlaneBasicPropertiesPage.js
+++ web-modeler/src/main/resources/META-INF/xhtml/js/m_swimlaneBasicPropertiesPage.js
@@ -168,25 +168,27 @@ define(
 								if(m_constants.CONDITIONAL_PERFORMER_PARTICIPANT_TYPE == participant.type){
 									var eventSymbols = this.getModelElement().diagram.eventSymbols;
 									for ( var n in eventSymbols) {
-										if (eventSymbols[n]
-												&& m_constants.NONE_EVENT_CLASS == eventSymbols[n].modelElement.eventClass) {
-											this.propertiesPanel.errorMessages
-											.push(m_i18nUtils
-													.getProperty("modeler.swimlane.properties.conditionalParticipant.manualTrigger.error"));
-											input.addClass("error");
-											input.focus();
-											this.propertiesPanel.showErrorMessages();
-											return false;
-										}
-										else if(m_constants.MESSAGE_EVENT_CLASS == eventSymbols[n].modelElement.eventClass
-												&& "scan" == eventSymbols[n].modelElement.implementation){
-											this.propertiesPanel.errorMessages
-											.push(m_i18nUtils
-													.getProperty("modeler.swimlane.properties.conditionalParticipant.scanTrigger.error"));
-											input.addClass("error");
-											input.focus();
-											this.propertiesPanel.showErrorMessages();
-											return false;
+										if(this.getModelElement().oid == eventSymbols[n].parentSymbol.oid){
+											if (eventSymbols[n]
+													&& m_constants.NONE_EVENT_CLASS == eventSymbols[n].modelElement.eventClass) {
+												this.propertiesPanel.errorMessages
+												.push(m_i18nUtils
+														.getProperty("modeler.swimlane.properties.conditionalParticipant.manualTrigger.error"));
+												input.addClass("error");
+												input.focus();
+												this.propertiesPanel.showErrorMessages();
+												return false;
+											}
+											else if(m_constants.MESSAGE_EVENT_CLASS == eventSymbols[n].modelElement.eventClass
+													&& "scan" == eventSymbols[n].modelElement.implementation){
+												this.propertiesPanel.errorMessages
+												.push(m_i18nUtils
+														.getProperty("modeler.swimlane.properties.conditionalParticipant.scanTrigger.error"));
+												input.addClass("error");
+												input.focus();
+												this.propertiesPanel.showErrorMessages();
+												return false;
+											}
 										}
 									}
 								}
