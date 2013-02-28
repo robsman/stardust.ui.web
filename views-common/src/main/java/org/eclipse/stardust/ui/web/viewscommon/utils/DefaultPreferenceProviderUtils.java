5f67e862c69020e6a89de341eb12dbc70586bef3
Sidharth Singh <sidharth.singh@sungard.com>
2013-02-28 06:00:01 +0000

    Jira-ID: CRNT-27828 Changed spring bean reference from "sungardSkinDefaultPreference" to "skinDefaultPreference" in DefaultPreferenceProviderUtils.java
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ui.web@63337 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- views-common/src/main/java/org/eclipse/stardust/ui/web/viewscommon/utils/DefaultPreferenceProviderUtils.java
+++ views-common/src/main/java/org/eclipse/stardust/ui/web/viewscommon/utils/DefaultPreferenceProviderUtils.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SunGard CSA LLC - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package org.eclipse.stardust.ui.web.viewscommon.utils;
 
 import org.eclipse.stardust.common.log.LogManager;
@@ -21,7 +31,7 @@ public class DefaultPreferenceProviderUtils
    {
       try
       {
-         String defaultSkin = (String) FacesUtils.getBeanFromContext("sungardSkinDefaultPreference");
+         String defaultSkin = (String) FacesUtils.getBeanFromContext("skinDefaultPreference");
          if (trace.isDebugEnabled())
          {
             trace.debug("Default Skin preference read from spring configuration is " + defaultSkin);
