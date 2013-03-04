0faeb768c64c4afe8b34fd36bddb94aae1f09e27
Sven Rottstock <sven.rottstock@sungard.com>
2013-03-04 16:34:42 +0100

    CRNT-27490: modified ivy descriptors so that only orbit artifacts are used
    Bug: 402291

--- business-control-center/antit.bat
+++ business-control-center/antit.bat
@@ -9,6 +9,18 @@
 @rem    SunGard CSA LLC - initial API and implementation and/or initial documentation
 @rem ***************************************************************************
 @echo off
+setlocal
 
-%CM_HOME%\bin\antit17.bat %*
+if "%ANT_HOME%"=="" (set ANT_HOME=.)
 
+if exist %ANT_HOME%\bin\ant.bat (set ANT_EXE=%ANT_HOME%\bin\ant.bat
+) else if exist %CM_HOME%\bin\antit17.bat (set ANT_EXE=%CM_HOME%\bin\antit17.bat
+) else (
+  echo Cannot find ANT executable. Please set environment variable: ANT_HOME & goto eof
+)
+
+%ANT_EXE% %*
+
+:eof
+
+endlocal
