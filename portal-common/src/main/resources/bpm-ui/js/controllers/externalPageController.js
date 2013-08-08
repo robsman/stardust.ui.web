90a85b30957a9ca1d5c6d12d333326fe7e903d77
Subodh Godbole <subodh.godbole@sungard.com>
2013-08-08 06:59:02 +0000

    Jira-ID: CRNT-29741 - Adjusted setting url to ui mashup frame only for IE case
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ui.web@66555 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Subodh Godbole <subodh.godbole@sungard.com>

--- portal-common/src/main/resources/bpm-ui/js/controllers/externalPageController.js
+++ portal-common/src/main/resources/bpm-ui/js/controllers/externalPageController.js
@@ -22,6 +22,20 @@ define(['bpm-ui/js/bpm-ui'], function (bpmUi) {
 			view.params["iframeId"] = $scope.iframeId;
 
 			$scope.label = view.label;
+
+			// In IE It's observed that url is hit twice. Add a workaround to get around this
+			// As this causes issues on server side due to two duplicate requests
+			// This might be Angular / HTML5 Framework issue
+			if (BridgeUtils.Util.isIE()) {
+				$scope.iframeUrl2 = $scope.iframeUrl;
+				$scope.iframeUrl = "about:blank";
+	
+				// Execute as soon as possible. No specific delay
+				window.setTimeout(function(){
+					var frame = window.document.getElementById($scope.iframeId);
+					frame.setAttribute("src", $scope.iframeUrl2);
+				});
+			}
 		} else {
 			log("View is null or it does not have Params. Something is incorrect. View = " + view);
 		}
