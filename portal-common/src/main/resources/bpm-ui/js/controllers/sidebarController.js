e48d1b9df9b308ec123daa19a79c5e421c76082d
Subodh Godbole <subodh.godbole@sungard.com>
2013-08-13 09:18:11 +0000

    Jira-ID: CRNT-29742 - There is no API to disable resizing, so added a workaround - However mouse cursor still changes to "resize" on hover, but resizing does not happen as expected
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ui.web@66648 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Subodh Godbole <subodh.godbole@sungard.com>

--- portal-common/src/main/resources/bpm-ui/js/controllers/sidebarController.js
+++ portal-common/src/main/resources/bpm-ui/js/controllers/sidebarController.js
@@ -9,10 +9,25 @@ define(['bpm-ui/js/bpm-ui'], function (bpmUi) {
 	 * 
 	 */
 	bpmUi.module.controller('bpm-ui.SidebarCtrl', ['$scope', function($scope) {
+		/*
+		 *
+		 */
+		function disableSidebarResizing() {
+			// There is no API to disable resizing, so add workaround
+			var sidebarResizeHandle = jQuery('.sidebarResizeHandle');
+			if (sidebarResizeHandle) {
+				sidebarResizeHandle.off();
+			}
+		}
+
 		// BridgeUtils not loaded at this point, so save these handlers in root for later use
 		$scope.$root.openSidebar = $scope.openSidebar;
 		$scope.$root.closeSidebar = $scope.closeSidebar;
 		$scope.$root.pinSidebar = $scope.pinSidebar;
 		$scope.$root.unpinSidebar = $scope.unpinSidebar;
+
+		$scope.$watch('sidebar.position', function(newValue) {
+			disableSidebarResizing();
+		}, true);
 	}]);
 });
