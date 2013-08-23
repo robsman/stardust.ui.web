37c91a4a240a7e54584d434427b1a76b578fa670
Shrikant Gangal <shrikant.gangal@sungard.com>
2013-08-23 08:15:20 +0000

    Jira-ID: CRNT-30029 Validation now gets triggered only on loss of focus and not on every character change (as before). This also solves the issue of server hit on every character change.
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ui.web@66907 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Shrikant Gangal <shrikant.gangal@sungard.com>

--- portal-common/src/main/resources/bpm-ui/js/bpm-ui.js
+++ portal-common/src/main/resources/bpm-ui/js/bpm-ui.js
@@ -17,6 +17,26 @@ define(function () {
 		sgViewPanelServiceProvider.setViewOpeningStrategy('mdi');
 	}]);
 
+	// Taken From - http://jsfiddle.net/cn8VF/
+	// This is to delay model updates till element is in focus
+	bpmUiRMod.module.directive('ngModelOnblur', function() {
+	    return {
+	        restrict: 'A',
+	        require: 'ngModel',
+	        link: function(scope, elm, attr, ngModelCtrl) {
+	            if (attr.type === 'radio' || attr.type === 'checkbox') {
+	            	return;
+	            }
+	            elm.unbind('input').unbind('keydown').unbind('change');
+	            elm.bind('blur', function() {
+	                scope.$apply(function() {
+	                    ngModelCtrl.$setViewValue(elm.val());
+	                });
+	            });
+	        }
+	    };
+	});
+	
 	angular.module('bpm-ui.init', []) .
 		run(['$log', '$rootScope', '$window', 'sgPubSubService', function ($log, $rootScope, $window, sgPubSubService) {
 			// execute here the code that needs to be executed before the app starts.
