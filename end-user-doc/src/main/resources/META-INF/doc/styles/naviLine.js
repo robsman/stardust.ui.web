659e78d86934bd1698e2ff6520c124a70b08848a
Monika Funke <monika.funke@sungard.com>
2013-08-27 09:21:59 +0000

    Jira-ID: CRNT-30296 adjusted end user doc style sheet to skip navigation to end user top for portal documentation
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ui.web@66956 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Monika Funke <monika.funke@sungard.com>

--- end-user-doc/src/main/resources/META-INF/doc/styles/naviLine.js
+++ end-user-doc/src/main/resources/META-INF/doc/styles/naviLine.js
@@ -86,12 +86,19 @@ function copyright()
 function line() {
 	document.write('<table class="header"><tr><td class="header">');
 	var top=0;
+	var startarray=0;
 	var uri = location.href;
 	var uriindex = uri.indexOf("/doc/");
 	var suburi = uri.substring(0,uriindex)+maindoclink;
-	document.write('<a href=',suburi,' class="header">Portal Documenation</a> &gt; ');
+	document.write('<a href=',suburi,' class="header">End User Documentation</a> &gt; ');
+//check if in Portal or bpm documentation
+	var checkindex = uri.indexOf("/portal/");
+	if (checkindex > 0) 
+		startarray=2;
+	else
+		startarray=0;
 
-	for (var i = 0; i < linkNumber; i=i+2) {
+	for (var i = startarray; i < linkNumber; i=i+2) {
   		document.write('<a href=',linkArray[i],' class="header">',linkArray[i+1],'</a> &gt; ');
 	}
 	document.write(document.title,'</td>');
