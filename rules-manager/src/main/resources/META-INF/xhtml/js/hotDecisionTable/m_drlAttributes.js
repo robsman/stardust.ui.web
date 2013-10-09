dde9c4b096684f744ecc56b5afae35795651e64b
Zachary McCain <zachary.mccain@sungard.com>
2013-10-09 13:03:47 +0000

    Jira-ID: CRNT-30646 - In support of,Internationalization. Added tooltip support for the addColumn tree-factory (Attributes)
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ui.web@67872 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Zachary McCain <zachary.mccain@sungard.com>

--- rules-manager/src/main/resources/META-INF/xhtml/js/hotDecisionTable/m_drlAttributes.js
+++ rules-manager/src/main/resources/META-INF/xhtml/js/hotDecisionTable/m_drlAttributes.js
@@ -1,55 +1,53 @@
-define([],function(){
+define(["rules-manager/js/m_i18nUtils"],function(m_i18nUtils){
 	var attributes={
 		"salience":{
 			type:"xsd:int",
 			defaultValue:0,
-			description:"Salience is a form of priority where rules with higher salience values" +
-						"are given higher priority when ordered in the Activation queue. Salience" +
-						"defaults to zero, and can be negative or positive."},
+			description:m_i18nUtils.getProperty("rules.propertyView.decisiontableview.dialog.addcolumn.tree.node.attribute.salience.tooltip","NA")},
 		"enabled":{
 			type:"xsd:boolean",
 			defaultValue:"false",
-			description:"A rule cannot activate if enabled is set to false."},
+			description: m_i18nUtils.getProperty("rules.propertyView.decisiontableview.dialog.addcolumn.tree.node.attribute.enabled.tooltip","NA")},
 		"date-effective":{
 			type:"xsd:date",
 			defaultValue:"",
-			description:"A rule can only activate if the current date and time is after date-effective attribute."},
+			description:m_i18nUtils.getProperty("rules.propertyView.decisiontableview.dialog.addcolumn.tree.node.attribute.date-effective.tooltip","NA")},
 		"date-expires":{
 			type:"xsd:date",
 			defaultValue:"",
-			description:"A rule cannot activate if the current date and time is after the date-expires attribute."},
+			description:m_i18nUtils.getProperty("rules.propertyView.decisiontableview.dialog.addcolumn.tree.node.attribute.date-expires.tooltip","NA")},
 		"no-loop":{
 			type:"xsd:boolean",
 			defaultValue:"false",
-			description:"Setting no-loop to true will skip the creation of another Activation for the rule with the current set of facts."},
+			description:m_i18nUtils.getProperty("rules.propertyView.decisiontableview.dialog.addcolumn.tree.node.attribute.no-loop.tooltip","NA")},
 		"agenda-group":{
 			type:"string",
 			defaultValue:"MAIN",
-			description:"Agenda groups allow the Agenda to be partitioned providing more execution control. Only rules in the agenda group that has acquired the focus are allowed to fire."},
+			description:m_i18nUtils.getProperty("rules.propertyView.decisiontableview.dialog.addcolumn.tree.node.attribute.agenda-group.tooltip","NA")},
 		"activation-group":{
 			type:"string",
 			defaultValue:"NA",
-			description:"Rules that belong to the same activation-group, identified by this attribute's string value, will only fire exclusively. More precisely, the first rule in an activation-group to fire will cancel all pending activations of all rules in the group, i.e., stop them from firing."},
+			description:m_i18nUtils.getProperty("rules.propertyView.decisiontableview.dialog.addcolumn.tree.node.attribute.activation-group.tooltip","NA")},
 		"duration":{
 			type:"xsd:double",
 			defaultValue:0,
-			description:"The duration dictates that the rule will fire after a specified duration, if it is still true."},
+			description:m_i18nUtils.getProperty("rules.propertyView.decisiontableview.dialog.addcolumn.tree.node.attribute.duration.tooltip","NA")},
 		"auto-focus":{
 			type:"xsd:boolean",
 			defaultValue:"false",
-			description:"When a rule is activated where the auto-focus value is true and the rule's agenda group does not have focus yet, then it is given focus, allowing the rule to potentially fire."},
+			description:m_i18nUtils.getProperty("rules.propertyView.decisiontableview.dialog.addcolumn.tree.node.attribute.auto-focus.tooltip","NA")},
 		"lock-on-active":{
 			type:"xsd:boolean",
 			defaultValue:"false",
-			description:"Whenever a ruleflow-group becomes active or an agenda-group receives the focus, any rule within that group that has lock-on-active set to true will not be activated any more; irrespective of the origin of the update, the activation of a matching rule is discarded."},
+			description:m_i18nUtils.getProperty("rules.propertyView.decisiontableview.dialog.addcolumn.tree.node.attribute.lock-on-active.tooltip","NA")},
 		"ruleflow-group":{
 			type:"string",
 			defaultValue:"NA",
-			description:"Rules that are assembled by the same ruleflow-group identifier fire only when their group is active."},
+			description:m_i18nUtils.getProperty("rules.propertyView.decisiontableview.dialog.addcolumn.tree.node.attribute.ruleflow-group.tooltip","NA")},
 		"dialect":{
 			type:"string",
 			defaultValue:"NA",
-			description:"Specifies the language to be used for any code expressions in the LHS or the RHS code block, i.e., Java or MVEL."}
+			description:m_i18nUtils.getProperty("rules.propertyView.decisiontableview.dialog.addcolumn.tree.node.attribute.dialect.tooltip","NA")}
 	};
 	return {
 		getAllAttributes: function(){return attributes;},
