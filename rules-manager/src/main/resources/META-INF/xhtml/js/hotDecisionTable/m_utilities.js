5bdbf5b090de3ddd8962343b5746defc0a889b03
Zachary McCain <zachary.mccain@sungard.com>
2013-10-24 13:34:26 +0000

    Jira-ID: CRNT-30646 - In support of, Standard format for UUIDs. Added a uuidV4 function which allows us to generate standard UUIDs as per section 4.4 of the UUID RFC-4122. - http://www.ietf.org/rfc/rfc4122.txt @see section 4.4
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ui.web@68324 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Zachary McCain <zachary.mccain@sungard.com>

--- rules-manager/src/main/resources/META-INF/xhtml/js/hotDecisionTable/m_utilities.js
+++ rules-manager/src/main/resources/META-INF/xhtml/js/hotDecisionTable/m_utilities.js
@@ -1,6 +1,17 @@
-/*Javascript implementation of Javas string.hashCode function*/
+/* uuidv4-UUID generator following version 4.4 of the spec.
+ *  	http://www.ietf.org/rfc/rfc4122.txt @see section 4.4
+		http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+ * hashString-Javascript implementation of Javas string.hashCode function
+ */
 define([],function(){
 	return {
+		uuidV4:function(){
+			  var uuid='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+			      return v.toString(16);
+			  });
+			  return uuid;
+			},
 		hashString: function(str){
 		    var hash = 0;
 		    if (str.length == 0) return hash;
