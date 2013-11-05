059918c9c2a8488c757839798e70a5d4298440b2
Zachary McCain <zachary.mccain@sungard.com>
2013-11-05 21:18:45 +0000

    Jira-ID: CRNT-31096 -In support of, Autocomplete functionality for parameterDefintions associated as facts to our RuleSet. sessionCOmpleters now store all their keyword within an user_extension property on our session object. SessionCompleters now require a key which it will attach to the user_extension property in order to store keyword collections as defined by the user.
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ui.web@68618 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Zachary McCain <zachary.mccain@sungard.com>

--- rules-manager/src/main/resources/META-INF/xhtml/js/m_autoCompleters.js
+++ rules-manager/src/main/resources/META-INF/xhtml/js/m_autoCompleters.js
@@ -2,8 +2,9 @@ define([],function(){
 	
 	var factory={
 			/*Completer which allows the user to specify a keyword list attached
-			 *to a  drlEditor session via session.ruleSetKeywords;*/
-			getSessionCompleter: function(options){
+			 *to a drlEditor session via session.ext_userDefined[key]
+			 *@Param: extKey - Hash key where we can find our keywords.*/
+			getSessionCompleter: function(extKey,options){
 				var metaName="Data",score=0;
 				if(options){
 					metaName=options.metaName || metaName;
@@ -11,7 +12,7 @@ define([],function(){
 				}
 				return {
 				    getCompletions: function(editor, session, pos, prefix, callback) {
-				        var keywords = session.ruleSetKeywords;
+				        var keywords = session.ext_userDefined[extKey];
 				        var t=session.getTextRange({
 				        	"start":{row: pos.row,column:pos.column-1},
 				        	"end":{row: pos.row,column:pos.column}
