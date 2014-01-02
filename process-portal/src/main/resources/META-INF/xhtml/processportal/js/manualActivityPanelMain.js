b1a95b052a3928b5777509141b7c11abe716be07
Subodh Godbole <subodh.godbole@sungard.com>
2014-01-02 12:18:19 +0000

    Jira-ID: CRNT-30681 - Using jquery.min instead of plain jquery
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ui.web@69765 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- process-portal/src/main/resources/META-INF/xhtml/processportal/js/manualActivityPanelMain.js
+++ process-portal/src/main/resources/META-INF/xhtml/processportal/js/manualActivityPanelMain.js
@@ -15,7 +15,7 @@
 require.config({
 	baseUrl: "../../plugins/",
 	paths: {
-		'jquery' : ['views-common/js/libs/jquery/jquery-1.7.2', '//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min'],
+		'jquery' : ['views-common/js/libs/jquery/jquery-1.7.2.min', '//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min'],
 		'jquery-ui': ['views-common/js/libs/jquery/plugins/jquery-ui-1.10.2.min', '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min'],
 		'angularjs' : ['views-common/js/libs/angular/angular-1.0.2', '//ajax.googleapis.com/ajax/libs/angularjs/1.0.2/angular.min'],
 		'json' : ['views-common/js/libs/json/json2', '//cdnjs.cloudflare.com/ajax/libs/json2/20110223/json2']
