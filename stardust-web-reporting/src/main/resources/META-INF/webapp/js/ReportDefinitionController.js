dd0d6dda5861d06e017f160968082d0d88a5f9d8
Aditya Gaikwad <aditya.gaikwad@sungard.com>
2014-04-25 05:42:15 +0000

    Jira-ID: CRNT-32708 Issue: Record Set >> Column Selector is blank initially. It works after saving the Report Definition. Fix: When new Report Definition is created, populated the available columns.
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ui.web@72159 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Aditya Gaikwad <aditya.gaikwad@sungard.com>

--- stardust-web-reporting/src/main/resources/META-INF/webapp/js/ReportDefinitionController.js
+++ stardust-web-reporting/src/main/resources/META-INF/webapp/js/ReportDefinitionController.js
@@ -607,6 +607,8 @@ define(
 						this.report.dataSet.fact = this.getPrimaryObject().facts.count.id;
 						this.report.dataSet.firstDimension = this
 								.getPrimaryObject().dimensions.processInstanceStartTimestamp.id;
+						
+						this.cumulatedDimensions = this.getCumulatedDimensions();
 
 						deferred.resolve();
 					}
