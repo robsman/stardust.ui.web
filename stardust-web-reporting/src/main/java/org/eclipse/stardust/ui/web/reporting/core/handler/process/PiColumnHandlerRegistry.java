e08377f249cd550aa790b5872f8d1856386e05e7
Holger Prause <holger.prause@sungard.com>
2014-04-28 18:39:17 +0000

    Jira-ID: CRNT-32703 added missing column handler to registry
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ui.web@72226 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-web-reporting/src/main/java/org/eclipse/stardust/ui/web/reporting/core/handler/process/PiColumnHandlerRegistry.java
+++ stardust-web-reporting/src/main/java/org/eclipse/stardust/ui/web/reporting/core/handler/process/PiColumnHandlerRegistry.java
@@ -20,6 +20,7 @@ public class PiColumnHandlerRegistry extends AbstractColumnHandlerRegistry<Proce
    public PiColumnHandlerRegistry()
    {
       register(new PiDescriptorColumnHandler());
+      register(PiDimensionField.OID.getId(), new PiOidColumnHandler());
       register(PiDimensionField.DURATION.getId(), new PiDurationColumnHandler());
       register(PiDimensionField.PROCESS_NAME.getId(), new PiNameColumnHandler());
       register(PiDimensionField.PRIORITY.getId(), new PiPriorityColumnHandler());
