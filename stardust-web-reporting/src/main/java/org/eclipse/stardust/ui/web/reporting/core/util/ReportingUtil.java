b639c9c9f0353eadf3f26bd1738554540231d31a
Holger Prause <holger.prause@sungard.com>
2014-05-07 15:55:22 +0000

    removed unused code
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ui.web@72461 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- stardust-web-reporting/src/main/java/org/eclipse/stardust/ui/web/reporting/core/util/ReportingUtil.java
+++ stardust-web-reporting/src/main/java/org/eclipse/stardust/ui/web/reporting/core/util/ReportingUtil.java
@@ -163,40 +163,4 @@ public class ReportingUtil
       startCalendar.setTime(startDate);
       return startCalendar.fieldDifference(endDate, unit.getCalendarField());
    }
-
-   /**
-    * Converts the key of a duration unit into the equivalent in seconds.
-    *
-    * @param unit
-    * @return
-    */
-   public static long convertDurationUnit(String unit)
-   {
-      if (unit.equals("m"))
-      {
-         return 1000 * 60;
-      }
-      else if (unit.equals("h"))
-      {
-         return 1000 * 60 * 60;
-      }
-      else if (unit.equals("d"))
-      {
-         return 1000 * 60 * 60 * 24;
-      }
-      else if (unit.equals("w"))
-      {
-         return 1000 * 60 * 60 * 24 * 7;
-      }
-      else if (unit.equals("M"))
-      {
-         return 1000 * 60 * 60 * 24 * 30; // TODO Consider calendar?
-      }
-      else if (unit.equals("Y"))
-      {
-         return 1000 * 60 * 60 * 24 * 30 * 256; // TODO Consider calendar?
-      }
-
-      throw new IllegalArgumentException("Duration unit \"" + unit + "\" is not supported.");
-   }
 }
