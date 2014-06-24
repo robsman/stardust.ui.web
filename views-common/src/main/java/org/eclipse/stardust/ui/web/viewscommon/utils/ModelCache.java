372dad3a8ef1c779f366289dfa1c6c7f34e9344e
Shrikant Gangal <shrikant.gangal@sungard.com>
2014-06-24 08:37:49 +0000

    Jira-ID: CRNT-33329 Modified isJsfContext() to not return true by default and actually check if faces context is available.
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ui.web@73465 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- views-common/src/main/java/org/eclipse/stardust/ui/web/viewscommon/utils/ModelCache.java
+++ views-common/src/main/java/org/eclipse/stardust/ui/web/viewscommon/utils/ModelCache.java
@@ -23,6 +23,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+import javax.faces.context.FacesContext;
+
 import org.eclipse.stardust.common.CollectionUtils;
 import org.eclipse.stardust.common.error.ObjectNotFoundException;
 import org.eclipse.stardust.common.log.LogManager;
@@ -497,8 +499,7 @@ public class ModelCache implements Resetable, Serializable, ModelResolver
 
    private static boolean isJsfContext()
    {
-      // TODO add real verification
-      return true;
+      return (FacesContext.getCurrentInstance() != null);
    }
 
    public static ModelCache findModelCache()
