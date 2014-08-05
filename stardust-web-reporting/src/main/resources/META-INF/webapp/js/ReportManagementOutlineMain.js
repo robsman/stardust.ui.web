361de91a2ff3795a4ba2b6edb27d102e1ed86805
Sidharth Singh <sidharth.singh@sungard.com>
2014-08-05 10:22:06 +0000

    Jira-ID: CRNT-31975 Added commonRequire.js to consolidate require config paths Used commonRequire.config() to load configs
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/branches/b_dev_8_0_x/stardust/ui.web@74043 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Sidharth Singh <sidharth.singh@sungard.com>

--- stardust-web-reporting/src/main/resources/META-INF/webapp/js/ReportManagementOutlineMain.js
+++ stardust-web-reporting/src/main/resources/META-INF/webapp/js/ReportManagementOutlineMain.js
@@ -1,38 +1,4 @@
-require
-		.config({
-			waitSeconds: 0,
-			baseUrl : "../../",
-			paths : {
-				'jquery' : [ 'bpm-reporting/public/js/libs/jquery/jquery-1.7.2',
-						'//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min' ],
-				'jquery-ui' : [
-						'bpm-reporting/public/js/libs/jquery/plugins/jquery-ui-1.10.2.custom.min',
-						'//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min' ],
-				'json' : [ 'bpm-reporting/public/js/libs/json/json2',
-						'//cdnjs.cloudflare.com/ajax/libs/json2/20110223/json2' ],
-				'angularjs' : [ 'bpm-reporting/public/js/libs/angular/angular-1.2.11',
-						'//ajax.googleapis.com/ajax/libs/angularjs/1.2.11/angular.min' ],
-				'jquery.base64' : [
-						'bpm-reporting/public/js/libs/jquery/plugins/jquery.base64',
-						'' ],
-				'jquery.jstree' : [
-						'bpm-reporting/public/js/libs/jquery/plugins/jquery.jstree',
-						'https://jstree.googlecode.com/svn-history/r191/trunk/jquery.jstree' ],
-				'i18n' : 'common/InfinityBPMI18N'
-			},
-			shim : {
-				'jquery-ui' : [ 'jquery' ],
-				'angularjs' : {
-					require : "jquery",
-					exports : "angular"
-				},
-				'i18n' : {
-					exports : "InfinityBPMI18N"
-				},
-				'jquery.base64' : [ 'jquery' ],
-				'jquery.jstree' : [ 'jquery' ]
-			}
-		});
+commonRequire.config({baseUrl: "../../"});
 
 require([ "require", "jquery", "jquery-ui", "json", "angularjs",
 		"jquery.base64", "jquery.jstree",
--- stardust-web-reporting/src/main/resources/META-INF/webapp/js/ReportStoragePopupMain.js
+++ stardust-web-reporting/src/main/resources/META-INF/webapp/js/ReportStoragePopupMain.js
@@ -1,25 +1,4 @@
-require
-		.config({
-			baseUrl : "../../../",
-			paths : {
-				'jquery' : [
-						'bpm-reporting/public/js/libs/jquery/jquery-1.7.2',
-						'//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min' ],
-				'angularjs' : [
-						'bpm-reporting/public/js/libs/angular/angular-1.2.11',
-						'//ajax.googleapis.com/ajax/libs/angularjs/1.2.11/angular.min' ],
-				'i18n' : 'common/InfinityBPMI18N',
-			},
-			shim : {
-				'angularjs' : {
-					require : "jquery",
-					exports : "angular"
-				},
-				'i18n' : {
-					exports : "InfinityBPMI18N"
-				}
-			}
-		});
+commonRequire.config({baseUrl: "../../../"});
 
 require([ "require", "jquery", "angularjs",
 		"bpm-reporting/js/ReportStorageController" ], function(require, jquery,
