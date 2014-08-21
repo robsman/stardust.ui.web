b38ee4bf5077b811f84ba6f9ee71a93c5e7e73ce
Robert Sauer <robert.sauer@sungard.com>
2014-08-21 17:33:52 +0200

    Copying over some code that was excluded from CRNT-34021 (in order to avoid risk)

--- stardust-business-object-management/src/main/java/org/eclipse/stardust/ui/web/business_object_management/service/BusinessObjectQueryEvaluator.java
+++ stardust-business-object-management/src/main/java/org/eclipse/stardust/ui/web/business_object_management/service/BusinessObjectQueryEvaluator.java
@@ -10,9 +10,33 @@
  *******************************************************************************/
 package org.eclipse.stardust.ui.web.business_object_management.service;
 
+import java.util.List;
+
 import org.eclipse.stardust.engine.api.model.IData;
 import org.eclipse.stardust.engine.api.query.AbstractDataFilter;
+import org.eclipse.stardust.engine.api.query.ActivityFilter;
+import org.eclipse.stardust.engine.api.query.ActivityInstanceFilter;
+import org.eclipse.stardust.engine.api.query.ActivityStateFilter;
+import org.eclipse.stardust.engine.api.query.BinaryOperatorFilter;
+import org.eclipse.stardust.engine.api.query.CurrentPartitionFilter;
+import org.eclipse.stardust.engine.api.query.DocumentFilter;
+import org.eclipse.stardust.engine.api.query.FilterCriterion;
 import org.eclipse.stardust.engine.api.query.FilterEvaluationVisitor;
+import org.eclipse.stardust.engine.api.query.FilterTerm;
+import org.eclipse.stardust.engine.api.query.ParticipantAssociationFilter;
+import org.eclipse.stardust.engine.api.query.PerformedByUserFilter;
+import org.eclipse.stardust.engine.api.query.PerformingOnBehalfOfFilter;
+import org.eclipse.stardust.engine.api.query.PerformingParticipantFilter;
+import org.eclipse.stardust.engine.api.query.PerformingUserFilter;
+import org.eclipse.stardust.engine.api.query.ProcessDefinitionFilter;
+import org.eclipse.stardust.engine.api.query.ProcessInstanceFilter;
+import org.eclipse.stardust.engine.api.query.ProcessInstanceHierarchyFilter;
+import org.eclipse.stardust.engine.api.query.ProcessInstanceLinkFilter;
+import org.eclipse.stardust.engine.api.query.ProcessStateFilter;
+import org.eclipse.stardust.engine.api.query.StartingUserFilter;
+import org.eclipse.stardust.engine.api.query.TernaryOperatorFilter;
+import org.eclipse.stardust.engine.api.query.UnaryOperatorFilter;
+import org.eclipse.stardust.engine.api.query.UserStateFilter;
 import org.eclipse.stardust.engine.core.persistence.Operator.Binary;
 import org.eclipse.stardust.engine.core.runtime.beans.ModelAwareQueryPredicate;
 
@@ -64,5 +88,152 @@ public class BusinessObjectQueryEvaluator extends ModelAwareQueryPredicate<IData
          return expected;
       }
       return super.getValue(data, attribute, expected);
+   }
+
+   public static class BinaryOperatorFilterValueExtractor extends AbstractExtractorVisitor
+   {
+      private final String attribute;
+
+      private final Binary operator;
+
+      public BinaryOperatorFilterValueExtractor(String attribute, Binary operator)
+      {
+         this.attribute = attribute;
+         this.operator = operator;
+      }
+
+      public Object visit(BinaryOperatorFilter filter, Object context)
+      {
+         if (operator.equals(filter.getOperator())
+               && attribute.equals(filter.getAttribute()))
+         {
+            return filter.getValue();
+         }
+         return null;
+      }
+   }
+
+   private static class AbstractExtractorVisitor implements FilterEvaluationVisitor
+   {
+      public Object visit(FilterTerm filter, Object context)
+      {
+         Object result = null;
+         List<FilterCriterion> parts = filter.getParts();
+         for (FilterCriterion criterion : parts)
+         {
+            Object value = criterion.accept(this, context);
+            if (value != null)
+            {
+               result = value;
+            }
+         }
+         // no parts means no restrictions
+         return result;
+      }
+
+      public Object visit(UnaryOperatorFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(BinaryOperatorFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(TernaryOperatorFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(ProcessDefinitionFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(ProcessStateFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(ProcessInstanceFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(StartingUserFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(ActivityFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(ActivityInstanceFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(ActivityStateFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(PerformingUserFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(PerformingParticipantFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(PerformingOnBehalfOfFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(PerformedByUserFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(AbstractDataFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(ParticipantAssociationFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(CurrentPartitionFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(UserStateFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(ProcessInstanceLinkFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(ProcessInstanceHierarchyFilter filter, Object context)
+      {
+         return null;
+      }
+
+      public Object visit(DocumentFilter filter, Object context)
+      {
+         return null;
+      }
    }
 }
