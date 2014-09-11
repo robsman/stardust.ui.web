dd316063c1cdd5edbc149106893fcf41bf933e5f
Gille, Marc <Marc.Gille@sungard.com>
2014-09-10 20:10:09 -0400

    Experiments with Properties Page.

--- stardust-business-object-management/src/main/resources/META-INF/webapp/js/BusinessObjectManagementDataPropertiesPage.js
+++ stardust-business-object-management/src/main/resources/META-INF/webapp/js/BusinessObjectManagementDataPropertiesPage.js
@@ -1,55 +1,73 @@
-define([
-  "bpm-modeler/js/m_utils",
-  "bpm-modeler/js/m_constants",
-  "bpm-modeler/js/m_commandsController",
-  "bpm-modeler/js/m_command",
-  "bpm-modeler/js/m_propertiesPage"
-], function(m_utils, m_constants, m_commandsController, m_command, m_propertiesPage) {
-  return {
-    create: function(propertiesPanel) {
-      return new BusinessObjectManagementDataPropertiesPage(propertiesPanel);
-    }
-  };
+define(
+		[ "bpm-modeler/js/m_utils", "bpm-modeler/js/m_constants",
+				"bpm-modeler/js/m_commandsController",
+				"bpm-modeler/js/m_command", "bpm-modeler/js/m_propertiesPage" ],
+		function(m_utils, m_constants, m_commandsController, m_command,
+				m_propertiesPage) {
+			return {
+				create : function(propertiesPanel) {
+					return new BusinessObjectManagementDataPropertiesPage(
+							propertiesPanel);
+				}
+			};
 
-  function BusinessObjectManagementDataPropertiesPage(newPropertiesPanel, newId, newTitle) {
+			function BusinessObjectManagementDataPropertiesPage(
+					newPropertiesPanel, newId, newTitle) {
+				// Inheritance
 
-    // Inheritance
+				var propertiesPage = m_propertiesPage.createPropertiesPage(
+						newPropertiesPanel,
+						"BusinessObjectManagementDataPropertiesPage",
+						"Business Object Management");
 
-    var propertiesPage = m_propertiesPage.createPropertiesPage(newPropertiesPanel,
-        "BusinessObjectManagementDataPropertiesPage", "Business Object Management");
+				m_utils.inheritFields(this, propertiesPage);
+				m_utils.inheritMethods(
+						BusinessObjectManagementDataPropertiesPage.prototype,
+						propertiesPage);
 
-    m_utils.inheritFields(this, propertiesPage);
-    m_utils.inheritMethods(BusinessObjectManagementDataPropertiesPage.prototype, propertiesPage);
+				this.templates = [];
 
-    // Field initialization
+				/**
+				 * 
+				 */
+				BusinessObjectManagementDataPropertiesPage.prototype.setElement = function() {
+					console.log(this);
+				};
 
-    this.primaryKeyFieldSelect = this.mapInputId("primaryKeyFieldSelect");
-    this.singleObjectRetrievalRestUriInput = this.mapInputId("singleObjectRetrievalRestUriInput");
-    this.objectSetRetrievalRestUriInput = this.mapInputId("objectSetRetrievalRestUriInput");
+				/**
+				 * 
+				 */
+				BusinessObjectManagementDataPropertiesPage.prototype.getData = function() {
+					return propertiesPanel.data;
+				};
 
-    this.registerInputForModelElementAttributeChangeSubmission(this.primaryKeyFieldSelect,
-        "checklist-management:primaryKeyField");
-    this.registerInputForModelElementAttributeChangeSubmission(this.singleObjectRetrievalRestUriInput,
-        "checklist-management:singleObjectRetrievalRestUriInput");
-    this.registerInputForModelElementAttributeChangeSubmission(this.objectSetRetrievalRestUriInput,
-        "checklist-management:objectSetRetrievalRestUriInput");
+				/**
+				 * 
+				 */
+				BusinessObjectManagementDataPropertiesPage.prototype.testMe = function() {
+					console.log("Test Me!");
+				};
 
-    /**
-     * 
-     */
-    BusinessObjectManagementDataPropertiesPage.prototype.setElement = function() {
-      this.primaryKeyFieldSelect.val(this.getModelElement().attributes["checklist-management:primaryKeyField"]);
-      this.singleObjectRetrievalRestUriInput
-          .val(this.getModelElement().attributes["checklist-management:singleObjectRetrievalRestUriInput"]);
-      this.objectSetRetrievalRestUriInput
-          .val(this.getModelElement().attributes["checklist-management:objectSetRetrievalRestUriInput"]);
-    };
+				/**
+				 * 
+				 */
+				BusinessObjectManagementDataPropertiesPage.prototype.validate = function() {
+					return true;
+				};
 
-    /**
-     * 
-     */
-    BusinessObjectManagementDataPropertiesPage.prototype.validate = function() {
-      return true;
-    };
-  }
-});
+				/**
+				 * 
+				 */
+				BusinessObjectManagementDataPropertiesPage.prototype.addTemplate = function() {
+					this.templates.push({});
+				};
+
+				/**
+				 * 
+				 */
+				BusinessObjectManagementDataPropertiesPage.prototype.removeTemplate = function(
+						index) {
+					this.templates.splice(index, 1);
+				};
+			}
+		});
