786b4b7301bcae90e39df887fe02f3c044ba8d22
Subodh Godbole <subodh.godbole@sungard.com>
2014-10-06 09:13:43 +0000

    Jira-ID: CRNT-34067 - Change API name
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/branches/b_dev_8_1_x/stardust/ui.web@75023 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- html5-common/src/main/resources/META-INF/webapp/scripts/main.js
+++ html5-common/src/main/resources/META-INF/webapp/scripts/main.js
@@ -4,4 +4,4 @@ angular.module('bpm-common.services', []);
 angular.module('bpm-common', ['bpm-common.services']);
 
 // Register top level module to Framework
-portalApplication.addModule('bpm-common');
+portalApplication.registerModule('bpm-common');
