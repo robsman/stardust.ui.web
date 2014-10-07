b3af0ad00b5f579400a5507e826822d3d84dccbd
Zachary McCain <zachary.mccain@sungard.com>
2014-10-07 20:55:31 +0000

    Jira-ID: CRNT-34434 - In support of, Common Dialogs
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ui.web@75108 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- html5-common/src/main/resources/META-INF/webapp/scripts/main.js
+++ html5-common/src/main/resources/META-INF/webapp/scripts/main.js
@@ -2,7 +2,10 @@
 // Top level module needs to have dependency on framework top level module called 'bpm-ui'
 angular.module('bpm-common.services', []);
 angular.module('bpm-common.directives', []);
-angular.module('bpm-common', ['bpm-common.services','bpm-common.directives']);
+angular.module('bpm-common', ['bpm-common.services', 'bpm-common.directives', 'ngDialog'])
+.config(['$httpProvider', function ($httpProvider) {
+	$httpProvider.interceptors.push('httpInterceptor');
+}]);
 
 // Register top level module to Framework
 portalApplication.addModule('bpm-common');
