21e8d8bed7cb52dfb0ad066f3e6d36206c397b2f
Zachary McCain <zachary.mccain@sungard.com>
2014-10-08 21:02:41 +0000

    Jira-ID: CRNT-34434 - In support of, Common Dialogs
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ui.web@75130 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- html5-common/src/main/resources/META-INF/webapp/scripts/directives/thirdParty/ngDialog/ngDialog.js
+++ html5-common/src/main/resources/META-INF/webapp/scripts/directives/thirdParty/ngDialog/ngDialog.js
@@ -373,34 +373,5 @@ module.directive('ngDialog', ['ngDialog', function (ngDialog) {
 	};
 }]);
 
-module.directive('ngDialogInfo', ['ngDialog', function (ngDialog) {
-	return {
-		restrict: 'A',
-		scope : {
-			ngDialogScope : '=',
-			ngOnOpen : '&sdOnOpen'
-		},
-		link: function (scope, elem, attrs) {
-			elem.on('click', function (e) {
-				e.preventDefault();
-
-				var ngDialogScope = angular.isDefined(scope.ngDialogScope) ? scope.ngDialogScope : 'noScope';
-				angular.isDefined(attrs.ngDialogClosePrevious) && ngDialog.close(attrs.ngDialogClosePrevious);
-
-				ngDialog.openConfirm({
-					template: "plugins/html5-common/scripts/directives/thirdParty/ngDialog/templates/info.html",
-					className: "ngdialog-theme-default",
-					controller: attrs.ngDialogController,
-					scope: ngDialogScope ,
-					onOpen: scope.ngOnOpen,
-					data: attrs.ngDialogData,
-					showClose: attrs.ngDialogShowClose === 'false' ? false : true,
-					closeByDocument: attrs.ngDialogCloseByDocument === 'false' ? false : true,
-					closeByEscape: attrs.ngDialogCloseByEscape === 'false' ? false : true
-				});
-			});
-		}
-	};
-}]);
 			
 		
