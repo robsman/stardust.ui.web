1710182d72561ee9ed5bdb87ce777c87fbbde673
Robert Sauer <robert.sauer@sungard.com>
2014-11-13 20:30:08 +0100

    Removing ngDialog reference.

--- html5-common/src/main/resources/META-INF/webapp/scripts/main.js
+++ html5-common/src/main/resources/META-INF/webapp/scripts/main.js
@@ -1,7 +1,7 @@
 // Define Custom Modules
 angular.module('bpm-common.services', []);
 angular.module('bpm-common.directives', []);
-angular.module('bpm-common', ['bpm-common.services', 'bpm-common.directives', 'ngDialog'])
+angular.module('bpm-common', ['bpm-common.services', 'bpm-common.directives'])
 .config(['$httpProvider', function ($httpProvider) {
 	$httpProvider.interceptors.push('httpInterceptor');
 }]);
