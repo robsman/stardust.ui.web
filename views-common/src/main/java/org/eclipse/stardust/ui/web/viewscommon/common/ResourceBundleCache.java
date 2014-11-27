c56d54fe251c90ad322c256c23aea881455bcd89
Roland Stamm <roland.stamm@sungard.com>
2014-11-27 13:37:54 +0000

    Jira-ID: CRNT-34849 Added copyright header.
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/branches/b_dev_8_1_x/stardust/ui.web@75701 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- views-common/src/main/java/org/eclipse/stardust/ui/web/viewscommon/common/ResourceBundleCache.java
+++ views-common/src/main/java/org/eclipse/stardust/ui/web/viewscommon/common/ResourceBundleCache.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SunGard CSA LLC - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 package org.eclipse.stardust.ui.web.viewscommon.common;
 
 import java.util.Locale;
@@ -5,7 +15,6 @@ import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import java.util.WeakHashMap;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 import org.eclipse.stardust.common.CollectionUtils;
