838048971a41294bd4caba9e3ede629b0ed57baa
Subodh Godbole <subodh.godbole@sungard.com>
2015-01-09 20:08:02 +0530

    CRNT-35186 - Revised implementation for standalone usage

--- html5-process-portal/src/main/resources/META-INF/webapp/standalone/html5Deps.js
+++ html5-process-portal/src/main/resources/META-INF/webapp/standalone/html5Deps.js
@@ -1,78 +1,124 @@
-/*
- * 
- * Utility for RequireJS way of loading things
- * Useful with arbitrary HTML page requiring utilities from HTML5 projects, like sd-data-table
- * 
- */
+var html5Deps = function() {
 
-var html5Deps = {
-	prepareRequireJsConfig : function(config) {
+	return {
+		loadStyleSheets : loadStyleSheets,
+		prepareRequireJsConfig : prepareRequireJsConfig,
+		bootstrapAngular : bootstrapAngular
+	};
+	
+	/**
+	 * Load datatable specific styles
+	 */
+	function loadStyleSheets(pluginBaseUrl) {
+		var head = document.getElementsByTagName('head')[0];
+		var styleSheets = [ "html5-common/styles/html5-common.css",
+				"html5-common/styles/sd-data-table.css",
+				"html5-common/styles/datatables/jquery.dataTables.css",
+				"html5-common/styles/font-awesome-4.2.0/css/font-awesome.css" ];
+
+		for ( var i in styleSheets) {
+			injectCSS(head, pluginBaseUrl + styleSheets[i]);
+		}
+	}
+	
+	/**
+	 * Update config with HTML5 dataTable paths,shims
+	 */	
+	function prepareRequireJsConfig(config) {
 		var reqMod = {
 			paths : {
 				'jquery.dataTables' : [ "html5-common/libs/datatables/1.10.2/main/jquery.dataTables" ],
 				'ngDialog' : [ 'html5-common/libs/ngDialog/ngDialog' ],
-				'angular-datatables' : [ 'business-object-management/js/libs/jquery/plugins/angular-datatables.min' ],
 				'portalApplication' : [ 'common/html5/portalApplication' ],
-				'main' : [ 'html5-common/scripts/main' ],
+				'html5CommonMain' : [ 'html5-common/scripts/main' ],
 				'sdEventBusService' : [ 'html5-common/scripts/services/sdEventBusService' ],
 				'httpInterceptorProvider' : [ 'html5-common/scripts/services/sdHttpInterceptorProvider' ],
 				'sdLoggerService' : [ 'html5-common/scripts/services/sdLoggerService' ],
 				'sdData' : [ 'html5-common/scripts/directives/sdData' ],
 				'sdDataTable' : [ 'html5-common/scripts/directives/sdDataTable' ],
 				'sdUtilService' : [ 'html5-common/scripts/services/sdUtilService' ],
-				'sdViewUtilService' : [ 'html5-common/scripts/services/sdViewUtilService' ]
+				'sdViewUtilService' : [ 'html5-common/scripts/services/sdViewUtilService' ],
+				'sdPreferenceService' : [ 'html5-common/scripts/services/sdPreferenceService' ]
 			},
 			shim : {
 				'jquery.dataTables' : [ 'jquery' ],
 				'ngDialog' : [ 'angularjs' ],
-				'main' : [ 'angularjs' ],
-				'sdEventBusService' : [ 'main' ],
-				'httpInterceptorProvider' : [ 'main' ],
-				'sdLoggerService' : [ 'main' ],
-				'sdData' : [ 'main' ],
-				'sdDataTable' : [ 'main', 'sdLoggerService' ],
-				'sdUtilService' : [ 'main' ],
-				'sdViewUtilService' : [ 'main' ]
+				'html5CommonMain' : [ 'angularjs' ],
+				'sdEventBusService' : [ 'html5CommonMain' ],
+				'httpInterceptorProvider' : [ 'html5CommonMain' ],
+				'sdLoggerService' : [ 'html5CommonMain' ],
+				'sdData' : [ 'html5CommonMain' ],
+				'sdDataTable' : [ 'html5CommonMain', 'sdLoggerService' ],
+				'sdUtilService' : [ 'html5CommonMain' ],
+				'sdViewUtilService' : [ 'html5CommonMain' ],
+				'sdPreferenceService' : [ 'html5CommonMain' ]
 			},
-			arrayObj : [ "jquery.dataTables", "ngDialog", "portalApplication",
-					"main", "sdEventBusService", "httpInterceptorProvider",
+			deps : [ "jquery.dataTables", "ngDialog", "portalApplication",
+					"html5CommonMain", "sdEventBusService", "httpInterceptorProvider",
 					"sdLoggerService", "sdData", "sdDataTable",
-					'sdUtilService', 'sdViewUtilService' ]
+					'sdUtilService', 'sdViewUtilService', 'sdPreferenceService' ]
 
 		};
 
-		this.concatenate(config.paths, reqMod.paths);
-		this.concatenate(config.shim, reqMod.shim);
-		config.arrayObj = config.arrayObj.concat(reqMod.arrayObj);
+		copyValues(config.paths, reqMod.paths);
+		copyValues(config.shim, reqMod.shim);
+		config.deps = config.deps.concat(reqMod.deps);
 		return config;
-	},
+	}
 
-	concatenate : function(a, b) {
-		for (property in b)
-			a[property] = b[property];
-
-		return a;
-	},
-
-	loadStyleSheets : function(pluginBaseUrl) {
-		var head = document.getElementsByTagName('head')[0];
-		var styleSheets = [
-			"html5-common/styles/html5-common.css",
-			"html5-common/styles/sd-data-table.css",
-			"html5-common/styles/datatables/jquery.dataTables.css",
-			"html5-common/styles/font-awesome-4.2.0/css/font-awesome.css"
-		];
-
-		for (var i in styleSheets) {
-			this.injectCSS(head, pluginBaseUrl + styleSheets[i]);
+	function bootstrapAngular(applicationModules) {
+		var module = angular.module("dummyBootstrapModule", []);
+		
+		module.provider('sgViewPanelService', function () {
+		      this.$get = ['$rootScope', function ($rootScope) {
+		          var service = {};
+		          return service;
+		      }];
+		  });
+		
+		  module.provider('sgPubSubService', function () {
+		      this.$get = ['$rootScope', function ($rootScope) {
+		          var service = {};
+		          service.subscribe = function() {
+		              
+		          };
+		          return service;
+		      }];
+		  });
+		  
+		var modules = portalApplication.getModules();
+		if (modules != null && modules.length > 0) {
+			modules = modules.concat(module.name);
+			if (modules[0] == 'bpm-ui') {
+				modules = modules.splice(1);
+			}
+		} else {
+			modules = [];
 		}
-	},
 
-	injectCSS : function(head, src) {
+		// Append application modules
+		modules = modules.concat(applicationModules);
+
+		angular.bootstrap(document, modules);  
+	}
+	/**
+	 * 
+	 */
+	function copyValues(dest, src) {
+		for (property in src) {
+			dest[property] = src[property];
+		}
+		return dest;
+	}
+	
+	/**
+	 * 
+	 */
+	function injectCSS(head, src) {
 		var link = document.createElement('link');
 		link.href = src;
 		link.rel = 'stylesheet';
 		link.type = 'text/css';
 		head.appendChild(link);
 	}
-};
+}();
