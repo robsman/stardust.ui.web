64cef548576abd75a50a9183afe12d55861468b5
Fradj Zayen <fradj.zayen@sungard.com>
2015-02-12 15:03:48 +0100

    [CRNT-35825]: Email Application-> exception is displayed if "Attach E-Mail to process" is checked
    Bug: CRNT-35825
    
    Signed-off-by: Fradj Zayen <fradj.zayen@sungard.com>

--- web-modeler/src/main/resources/META-INF/xhtml/js/MailIntegrationOverlay.js
+++ web-modeler/src/main/resources/META-INF/xhtml/js/MailIntegrationOverlay.js
@@ -1197,6 +1197,11 @@ define(
                         valid = false;
                      }
                   }
+                  if(this.identifierInSubjectInput.prop("checked") && this.storeEmailInput.prop("checked")){
+                     this.view.errorMessages.push("You cannot Attach E-Mail to process when Include Unique Identifier is checked."); 
+                     this.storeEmailInput.addClass("error");
+                     valid = false;
+                  }
                   return valid;
                };
                this.sourceOptions = [ {
