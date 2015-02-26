615607a5787dc23bcf95b3ee11eaf6e6b171704d
Subodh Godbole <subodh.godbole@sungard.com>
2015-02-26 15:55:53 +0530

    Correcting Typo in filename (Capital v/s Small)

--- html5-process-portal/src/main/resources/META-INF/webapp/scripts/directives/sdProcessActivityFilter.js
+++ html5-process-portal/src/main/resources/META-INF/webapp/scripts/directives/sdProcessActivityFilter.js
@@ -27,7 +27,7 @@
 
       return {
          restrict : 'A',
-         templateUrl : 'plugins/html5-process-portal/scripts/directives/partials/ProcessActivityFilter.html',
+         templateUrl : 'plugins/html5-process-portal/scripts/directives/partials/processActivityFilter.html',
          controller : [ '$scope', '$attrs', '$filter','$parse', FilterController ],
          link : function(scope, element, attr, ctrl)
          {
