919a4d5354dbd5189f55447d5837a8f5f1e4ec9b
Sidharth Singh <sidharth.singh@sungard.com>
2015-03-06 17:15:16 +0530

    CRNT-35186 Added css dependencies Made change to jquery path Removed font-awesome reference

--- html5-process-portal/src/main/resources/META-INF/webapp/standalone/html5Deps.js
+++ html5-process-portal/src/main/resources/META-INF/webapp/standalone/html5Deps.js
@@ -11,10 +11,28 @@ var html5Deps = function() {
 	 */
 	function loadStyleSheets(pluginBaseUrl) {
 		var head = document.getElementsByTagName('head')[0];
-		var styleSheets = [ "html5-common/styles/html5-common.css",
+		var styleSheets = [
+				"html5-common/styles/3rd-party/jquery-plugins/jquery-ui/1.10.2/jquery-ui-custom.css",
+				"html5-common/styles/3rd-party/glyphicons/glyphicons.css",
+				"html5-common/styles/starclipse/starclipse.css",
+				"html5-common/styles/3rd-party/datatables/1.9.4/jquery.dataTables.css",
+				"html5-common/styles/3rd-party/bootstrap/bootstrap-theme.css",
+				"html5-common/styles/3rd-party/bootstrap/bootstrap-modal.css",
 				"html5-common/styles/sd-data-table.css",
-				"html5-common/styles/datatables/jquery.dataTables.css",
-				"html5-common/styles/font-awesome-4.2.0/css/font-awesome.css" ];
+				"html5-common/styles/sd-autoComplete.css",
+				"html5-common/styles/html5-common.css",
+				"html5-common/styles/glyphicons-ext.css",
+				"html5-common/styles/starclipse/starclipse.css",
+				"html5-common/styles/3rd-party/jquery-plugins/jquery-ui/1.10.2/jquery-ui-custom.css",
+				"html5-common/styles/3rd-party/glyphicons/glyphicons.css",
+				"html5-common/styles/3rd-party/datatables/1.9.4/jquery.dataTables.css",
+				"html5-common/styles/3rd-party/bootstrap/bootstrap-theme.css",
+				"html5-common/styles/3rd-party/bootstrap/bootstrap-modal.css",
+				"html5-common/styles/sd-data-table.css",
+				"html5-common/styles/sd-autoComplete.css",
+				"html5-common/styles/html5-common.css",
+				"html5-common/styles/glyphicons-ext.css",
+				"html5-process-portal/styles/html5-process-portal.css" ];
 
 		for ( var i in styleSheets) {
 			injectCSS(head, pluginBaseUrl + styleSheets[i]);
@@ -27,7 +45,7 @@ var html5Deps = function() {
 	function prepareRequireJsConfig(config) {
 		var reqMod = {
 			paths : {
-				'jquery.dataTables' : [ "html5-common/libs/datatables/1.10.2/main/jquery.dataTables" ],
+				'jquery.dataTables' : [ "html5-common/libs/datatables/1.9.4/jquery.dataTables" ],
 				'portalApplication' : [ 'common/html5/portalApplication' ],
 				'html5CommonMain' : [ 'html5-common/scripts/main' ],
 				'sdEventBusService' : [ 'html5-common/scripts/services/sdEventBusService' ],
@@ -79,6 +97,21 @@ var html5Deps = function() {
 		          var service = {};
 		          service.subscribe = function() {
 		              
+		          };
+		          return service;
+		      }];
+		  });
+		  
+		  // TODO - temp fix applied, resolve using angularContext
+		  module.provider('sgI18nService', function () {
+		      this.$get = ['$rootScope', function ($rootScope) {
+		          var service = {};
+		          service.translate = function(key, value) {
+		              if(value){
+		            	  return value;
+		              }else{
+		            	  return key;
+		              }
 		          };
 		          return service;
 		      }];
