a6304588be63e7e15b9c2d36a4f3ee6d2696e7be
Aditya Gaikwad <aditya.gaikwad@sungard.com>
2015-04-01 12:01:00 +0530

    [CRNT-36426]: Implemented refresh view toolbar AngularJS directive.
    Bug: CRNT-36426

---
+++ html5-process-portal/src/main/resources/META-INF/webapp/scripts/directives/sdRefresh.js
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2014 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SunGard CSA LLC - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
+/**
+ * @author Aditya.Gaikwad
+ */
+
+(function() {
+	'use strict';
+
+	angular.module('bpm-common').directive('sdRefresh', [ RefreshDirective ]);
+
+	/*
+	 * 
+	 */
+	function RefreshDirective() {
+		return {
+			restrict : 'AE',
+			scope : {
+				refresh : "&sdaOnRefresh"
+			},
+			templateUrl : 'plugins/html5-process-portal/scripts/directives/partials/refreshViewToolbar.html',
+			controller : [ '$scope', RefreshController ]
+		};
+	}
+
+	/**
+	 * 
+	 */
+	function RefreshController($scope) {
+		// Make sure i18n is available in the current scope
+		if (!angular.isDefined($scope.i18n)) {
+			this.i18n = $scope.$parent.i18n;
+		}
+		$scope.refreshController = this;
+	}
+
+})();
