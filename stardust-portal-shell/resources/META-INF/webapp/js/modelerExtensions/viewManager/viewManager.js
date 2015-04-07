3c05b2c1a626d9230acf67e60b884182b1f45bc2
Robert Sauer <robert.sauer@sungard.com>
2015-04-07 15:56:45 +0200

    Removing obsolete project stardust-portal-shell
    Jira-ID: CRNT-35971

--- stardust-portal-shell/resources/META-INF/webapp/js/modelerExtensions/viewManager/viewManager.js
+++
@@ -1,8 +0,0 @@
-//define([ '../../../../startdust-portal-shell/js/ViewManager' ], function(ViewManager) {
-//	return {
-//		viewManager: {
-//			id : "htmlViewManager",
-//			provider : ViewManager,
-//		},
-//	};
-//});
