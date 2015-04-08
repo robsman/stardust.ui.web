eeda98c8c248b69b8cf2e396a8c8440d8d931ab4
Aditya Gaikwad <aditya.gaikwad@sungard.com>
2015-04-08 16:27:34 +0530

    [CRNT-35802]: Handled the window console object which was failing in IE9
    Bug: CRNT-35802

--- views-common/src/main/resources/META-INF/xhtml/js/commonRequire.js
+++ views-common/src/main/resources/META-INF/xhtml/js/commonRequire.js
@@ -7,6 +7,25 @@ if (!window.bpm.portal) {
 }
 
 if (!window.bpm.portal.viewsCommonRequire) {
+	
+	(function() {
+		  if (!window.console) {
+		    window.console = {};
+		  }
+		  // union of Chrome, FF, IE, and Safari console methods
+		  var m = [
+		    "log", "info", "warn", "error", "debug", "trace", "dir", "group",
+		    "groupCollapsed", "groupEnd", "time", "timeEnd", "profile", "profileEnd",
+		    "dirxml", "assert", "count", "markTimeline", "timeStamp", "clear"
+		  ];
+		  // define undefined methods as noops to prevent errors
+		  for (var i = 0; i < m.length; i++) {
+		    if (!window.console[m[i]]) {
+		      window.console[m[i]] = function() {};
+		    }    
+		  } 
+		})();
+	
 	bpm.portal.viewsCommonRequire = {
 		PATHS : {
 			'jquery' : [ 'views-common/js/libs/jquery/jquery-1.7.2.min',
