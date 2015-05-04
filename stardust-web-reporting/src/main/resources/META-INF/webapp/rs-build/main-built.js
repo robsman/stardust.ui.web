c3045602b3c32ef699dbc29938e662119c6fcb50
Aditya Gaikwad <aditya.gaikwad@sungard.com>
2015-05-04 17:33:18 +0530

    [CRNT-36695]: Added Stardust copyright header message to all js and css files where it's missing (excluding thirdparty)
    Bug: CRNT-36695

--- stardust-web-reporting/src/main/resources/META-INF/webapp/rs-build/main-built.js
+++ stardust-web-reporting/src/main/resources/META-INF/webapp/rs-build/main-built.js
@@ -1,3 +1,14 @@
+
+/*******************************************************************************
+ * Copyright (c) 2015 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     SunGard CSA LLC - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 /*! jQuery v1.7.2 jquery.com | jquery.org/license */
 
 /*! jQuery UI - v1.10.2 - 2013-03-14
