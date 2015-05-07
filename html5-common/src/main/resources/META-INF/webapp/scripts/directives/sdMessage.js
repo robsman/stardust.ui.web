93d35e3ff3a15b2e10c97d657febecd60d06ae51
Nikhil Gahlot <nikhil.gahlot@sungard.com>
2015-05-07 18:18:50 +0530

    [CRNT-36881]: Created a directive and a service for showing error/validation message.
    Bug: CRNT-36881
    
    Conflicts:
    	html5-common/src/main/resources/html5-common_en.properties
    
    Signed-off-by: Nikhil Gahlot <nikhil.gahlot@sungard.com>

---
+++ html5-common/src/main/resources/META-INF/webapp/scripts/directives/sdMessage.js
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * Copyright (c) 2011 SunGard CSA LLC and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: SunGard CSA LLC - initial API and implementation and/or initial
+ * documentation
+ ******************************************************************************/
+
+angular
+		.module('bpm-common.directives')
+		.directive(
+				'sdMessage',
+				[
+						'eventBus',
+						function(eventBus) {
+							return {
+								scope : {
+									messageKey : '@sdMessage'
+								},
+								template : '<table ng-show="showError" style="width : 100%">'
+										+ '<tr>'
+											+ '<td style="width : 1em ; align:center">'
+												+ '<i ng-if="type == \'error\'" class="glyphicon glyphicon-remove-sign msg-{{type}}"></i>'
+												+ '<i ng-if="type == \'info\'" class="glyphicon glyphicon-exclamation-sign msg-{{type}}"></i>'
+												+ '<i ng-if="type == \'warn\'" class="glyphicon glyphicon-exclamation-sign msg-{{type}}"></i>'
+											+ '</td>'
+											+ '<td><span ng-bind="message" class="msg-{{type}}"></span</td>'
+										+ '</tr>'
+										+ '</table>',
+								link : function($scope) {
+
+									var initScope = function(e, m) {
+										console.log(m);
+										
+										$scope.message = m;
+										if (angular.isObject($scope.message)) {
+											$scope.type = m.type;
+											$scope.message = m.message;
+										}
+										
+										if (!angular.isDefined($scope.type)) {
+											$scope.type = 'error';
+										}
+										
+										$scope.showError = true;
+									};
+
+									var resetScope = function(e, m) {
+										console.log(m);
+
+										$scope.type = undefined;
+										$scope.message = undefined;
+										
+										$scope.showError = false;
+									};
+									
+									if (!angular.isDefined($scope.messageKey)) {
+										$scope.messageKey = 'global.error';
+									}
+
+									eventBus.onMsg($scope.messageKey, initScope, $scope);
+
+									eventBus.onMsg($scope.messageKey + ".reset", resetScope, $scope);
+								}
+							};
+						} ]);
---
+++ html5-common/src/main/resources/META-INF/webapp/scripts/services/sdMessageService.js
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2014 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SunGard CSA LLC - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
+(function(){
+	'use strict';
+
+	angular.module('bpm-common.services').factory('sdMessageService', ['eventBus', function (eventBus) {
+		
+		function emitMessage(message, namespace) {
+			if (!angular.isDefined(namespace)) {
+				namespace = 'global.error';
+			}
+			eventBus.emitMsg(namespace, message);
+		}
+		
+		return {
+			
+			/**
+			 * message can be a string or an object {message: <message>, type: <type>}
+			 * Supported type values : 'error' / 'info' / 'warn'
+			 */
+			showMessage: function(message, namespace) {
+				emitMessage(message, namespace);
+			}
+		};
+	}]);
+})();
