07d533f8909e4bbd65a920b42774ee7e76d3a311
Anoop Nair <anoop.nair@sungard.com>
2015-05-13 16:14:51 -0500

    [CRNT-36946]: Added fix for individual descriptor not getting fetched
    when descriptors not selected.Caused due to change in the name column
    for descriptors.
    
    Bug: CRNT-36946
    
    (cherry picked from commit 53db2638f6918c53fdc5d2c9b56612ae6ec966be)
    
    Signed-off-by: Johnson Quadras <johnson.quadras@sungard.com>

--- html5-process-portal/src/main/resources/META-INF/webapp/scripts/directives/sdWorklist.js
+++ html5-process-portal/src/main/resources/META-INF/webapp/scripts/directives/sdWorklist.js
@@ -578,6 +578,7 @@
 	    self.cleanLocals();
 
 	    var query = angular.extend({}, this.query);
+	    options.descriptorColumns = self.descritorCols;
 	    query.options = options;
 
 	    if (angular.isDefined(this.sdDataCtrl)) {
