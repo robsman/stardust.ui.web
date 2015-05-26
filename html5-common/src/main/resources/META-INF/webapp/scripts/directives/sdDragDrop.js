a03d97e733b7032deda3e1ce3c2145a2fed21297
McCain, Zach <zachary.mccain@sungard.com>
2015-05-26 15:11:59 -0500

    [CRNT-34443]: HTMl5 Components - Drag Drop
    Updated code to sniff browsers in order to detect IE which has a slightly different DataTransfer API which we must account for.
    
    Bug: CRNT-34443

--- html5-common/src/main/resources/META-INF/webapp/scripts/directives/sdDragDrop.js
+++ html5-common/src/main/resources/META-INF/webapp/scripts/directives/sdDragDrop.js
@@ -8,6 +8,112 @@
  * Contributors:
  *     SunGard CSA LLC - initial API and implementation and/or initial documentation
  *******************************************************************************/
+(function(){
+	
+	//Sniff browser so we can set the proper dataType on the dataTransfer API
+	var dataType = "text/plain";
+	
+	if(navigator.appName==="Microsoft Internet Explorer"){
+		dataType = "text";
+	}
+	
+	/**
+	 * sdDataDrop: makes the associated element a drop target with
+	 * a user defined callback for drop events - attrs.onDrop
+	 */
+	angular.module('bpm-common.directives')
+	.directive("sdDataDrop",function($parse){
+		
+	   return{
+	    scope: true,
+	    link: function(scope, elem, attrs){
+	      
+	      elem.attr("droppable","true");
+
+	      elem.bind("dragenter",function(e){
+	    	console.log("Drag-enter");
+	        e.preventDefault();
+	        return true;
+	      });
+	      
+	      elem.bind("dragover",function(e){
+	          if (e.preventDefault) {e.preventDefault();}
+	          if (e.stopPropagation) {e.stopPropagation();}
+	          e = (e.dataTransfer)?e: e.originalEvent;
+	          e.dataTransfer.dropEffect = 'move';
+	          console.log("Drag-over");
+	          return false;
+	      });
+	      
+	      elem.bind("drag",function(){
+	    	console.log("Drag");
+	        return false;
+	      });
+	      
+	      elem.bind("drop",function(e){
+	        if (e.stopPropagation) {e.stopPropagation();}
+	        e = (e.dataTransfer)?e: e.originalEvent;
+	        var data = e.dataTransfer.getData(dataType);
+	        var fn = $parse(attrs.sdaDrop);
+	        fn(scope, {$data: data, $event: e});
+	        console.log("Drop");
+	      });
+	    }
+	  };
+	});//sd-data-drop end
+	
+	
+	/**
+	 * sdDataDrag: makes the associated element draggable with
+	 * a user defined callback for drop events - attrs.onDrop
+	 */
+	angular.module('bpm-common.directives')
+	.directive("sdDataDrag",function($parse){
+		
+	  return{
+	    require: "ngModel",
+	    link: function(scope, elem, attrs,ngModelCtrl){
+	      
+	      var data=ngModelCtrl.$viewValue;
+	      
+	      elem.attr("draggable","true");
+
+	      elem.bind("dragend",function(e){
+	        var fn = $parse(attrs.sdaDragend);
+	        
+	        srcObject=ngModelCtrl.$modelValue;
+	        fn(scope, {$data: srcObject, $event: e});
+	        console.log("Drag-end");
+	        
+	      });
+
+	      elem.bind("dragstart",function(e){
+	    	  
+	        var data =ngModelCtrl.$viewValue,
+	        	fn = $parse(attrs.sdaDragstart);
+	        
+	        //DragStart callback
+	        data = fn(scope, {$data: ngModelCtrl.$viewValue, $event: e}) || data;
+	        data = JSON.stringify(data);
+	        
+	        e = (e.dataTransfer)?e: e.originalEvent;
+	        e.dataTransfer.setData(dataType,data);
+	        e.dataTransfer.dropEffect = 'move';
+	        if(e.dataTransfer.setDragImage){
+	        	e.dataTransfer.setDragImage(elem[0],0,0);
+	        }
+	        e.dataTransfer.effectAllowed="move";
+
+	      });
+	      
+	    }
+	  };
+	});//sd-data-drag end
+	
+})();
+
+//TODO: REMOVE OLD IMPLEMENTATION
+/*
 angular.module('bpm-common.directives')
 .directive("sdDataDrop",function($parse){
    return{
@@ -77,3 +183,4 @@ angular.module('bpm-common.directives')
     }
   };
 });
+*/
