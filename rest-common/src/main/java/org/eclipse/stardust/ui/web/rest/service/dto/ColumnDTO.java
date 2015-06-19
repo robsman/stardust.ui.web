f18f3b64017fdfce5617b1571e6e306223dea4e8
Johnson Quadras <johnson.quadras@sungard.com>
2015-06-19 14:01:15 +0530

    [CRNT-35275] : Resolved conflicts. Bug: CRNT-35275
    Signed-off-by: Johnson Quadras <johnson.quadras@sungard.com>

--- rest-common/src/main/java/org/eclipse/stardust/ui/web/rest/service/dto/ColumnDTO.java
+++ rest-common/src/main/java/org/eclipse/stardust/ui/web/rest/service/dto/ColumnDTO.java
@@ -1,40 +1,3 @@
-<<<<<<< Upstream, based on origin/feature/ipp/portal-html5-contrib
-/*******************************************************************************
- * Copyright (c) 2014 SunGard CSA LLC and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    SunGard CSA LLC - initial API and implementation and/or initial documentation
- *******************************************************************************/
-package org.eclipse.stardust.ui.web.rest.service.dto;
-
-
-/**
- * 
- * @author Johnson.Quadras
- *
- */
-public class ColumnDTO extends AbstractDTO
-{
-
-   public String id;
-   
-   public String label;
-
-   public ColumnDTO(String id, String label)
-   {
-      super();
-      this.id = id;
-      this.label = label;
-   }
-   
-   
-
-}
-=======
 /*******************************************************************************
  * Copyright (c) 2014 SunGard CSA LLC and others.
  * All rights reserved. This program and the accompanying materials
@@ -68,4 +31,3 @@ public class ColumnDTO extends AbstractDTO
    }
 
 }
->>>>>>> 30ff258 [CRNT-35275] : Added screen for postponed activities view. Bug: CRNT-35275
