7a0fdfd5395e0baa02e675d67a4ad9cbf12c4119
McCain, Zach <zachary.mccain@sungard.com>
2015-08-18 10:43:49 -0500

    [CRNT-37975]: Portals: Tree component to display a folder / file structure from the Document Repository.
    Initial check in for sdFolderTree directive. Currently read-only and supports multi versus single select of document nodes (cannot select folder nodes). Events published through the onInit callback = document-select-multi,document-select-single,document-deselect.
    Selected nodes are available via api function (getSelectedNodes)
    
    Bug: CRNT-37975

---
+++ html5-common/src/main/resources/META-INF/webapp/scripts/directives/sdFolderTree/sdFolderTree.js
@@ -0,0 +1,279 @@
+/*******************************************************************************
+ * Copyright (c) 2014 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SunGard CSA LLC - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
+/**
+ * Provides an implementation of a folder browser wired to a rest common end point
+ * representing our IPP document repository.
+ * Initial path can be set using the @sdaRootPath attribute on the directive.
+ * If no path is given the rootpath with be the base folder of the document repository.
+ * 
+ */
+(function(){
+  
+  var mod = angular.module("bpm-common.directives");
+  
+  function sdFolderService($http,$q,sdUtilService){
+    this.$http = $http;
+    this.$q = $q;
+    this.documentCount=0;
+    this.folderCount=0;
+    this.rootUrl = sdUtilService.getRootUrl();
+  };
+  
+  //returns {documents:[],folders:[]}
+  sdFolderService.prototype.getFolders = function(path){
+    
+    var that = this,
+    	url,
+        deferred = this.$q.defer();
+
+    path = path || "";
+    
+    url = this.rootUrl + "/services/rest/portal/folder/" + path;
+    this.$http.get(url)
+    .then(function(res){
+    	deferred.resolve(res.data);
+    })
+    ["catch"](function(err){
+    	deferred.reject(err);
+    });
+
+    return deferred.promise;
+    
+  };
+  
+  sdFolderService.$inject=["$http", "$q", "sdUtilService"];
+  
+  mod.service("sdFolderService",sdFolderService);
+  
+  /**
+   * Controller function for Directive
+   */
+  function sdFolderTreeController(folderService,$q,$scope){
+	var that = this;
+	
+    this.$q = $q;
+    this.$scope = $scope;
+    this.folderService = folderService;
+    this.loadRootFolder($scope.rootPath || "");//loading top level tree structure
+    this.folderData = [];
+    this.selectedNodes=[];
+    
+    //Setup multi-select property based on scope attribute
+    $scope.allowMultiselect = $scope.allowMultiselect || "";
+    $scope.allowMultiselect = $scope.allowMultiselect.toUpperCase();
+    this.allowMultiselect = $scope.allowMultiselect==="TRUE";
+    
+    //Set up api to return to any listeners.
+    this.api = {
+      getSelectedNodes : function(){
+        return that.selectedNodes;
+      }
+    };
+    that.$scope.onInit({api:that.api});
+    
+  };
+  
+  sdFolderTreeController.prototype.iconCallback = function(item){
+   
+    if(item.itemType==="document"){
+		if(this.selectedNodes.some(function(v){return v.valueItem.uuid === item.uuid;})){
+		    return "fa fa-file selected";
+		}
+		else{
+			return "fa fa-file";
+		}
+		
+    }
+    
+    if(item.itemType==="folder"){
+      return "fa fa-folder";
+    }
+  
+    return "fa fa-folder";
+  };
+  
+  sdFolderTreeController.prototype.iconChildCallback = function(item){
+
+    if(item.itemType==="document"){
+      return "fa fa-cog";
+    }
+    
+    if(item.itemType==="folder"){
+      return "fa fa-folder";
+    }
+    
+
+  };
+  
+  
+  /**
+   * Handle our tree events here. 
+   * @param data
+   * @param e
+   */
+  sdFolderTreeController.prototype.eventCallback = function(data,e){
+
+    data.deferred.resolve();
+    
+    var i, 
+    	tempNode,
+    	docIndex,
+        that = this;
+        
+    this.selectedItem  = data.valueItem;
+    this.lastEvent = data.treeEvent;
+    docIndex = -1;
+    
+    //On expansion of a node we need to build out its children which means
+    //if the node has not been previously expanded we need to leverage our 
+    //service to instantiate its child folders and documents.
+    if(data.treeEvent==="node-expand"){
+      
+      //If item has already been loaded or is a document then return
+      if(data.valueItem.isLoaded===true || data.valueItem.itemType==="document"){
+        data.deferred.resolve();
+        return;
+      }
+      else{
+        that.folderService.getFolders(data.valueItem.path)
+        .then(function(children){
+          data.valueItem.items =  that.normalizeFolderData(children).items;
+          data.valueItem.isLoaded = true;
+        });
+      }
+    }
+    
+    //Nothing to do on node collapse so just resolve
+    else if(data.treeEvent==="node-collapse"){
+       data.deferred.resolve();
+    }
+    
+    //For nodeclicks on a document we need to add the node to our selectedNodes collection,
+    //unless the node is already present in which case we remove it. Single select
+    //unless the ctrl key is depressed in which case we use multi-select logic
+    else if (data.treeEvent ==="node-click" && data.valueItem.itemType==="document"){
+        
+    	//Find existing index (if any) of item clicked
+		for(i=0;i<that.selectedNodes.length;i++){
+		  tempNode = that.selectedNodes[i];
+		  if(tempNode.valueItem.uuid === data.valueItem.uuid){
+		    docIndex = i;
+		    break;
+		  }
+		}
+		
+		//if found on selectedNodes then remove
+		if(docIndex > -1){
+		  that.selectedNodes.splice(docIndex,1);
+		  this.$scope.eventCallback({item:{
+			  eventType: "document-deselected",
+			  document: data.valueItem
+		  }});
+		}
+		//not on selected nodes but as ctrlKey is pressed we push onto
+		//the array without emptying it first (multi-select)
+		else if(e.ctrlKey && this.allowMultiselect){
+		 that.selectedNodes.push(data);
+		 this.$scope.eventCallback({item:{
+			  eventType: "document-selected-multi",
+			  document: data.valueItem
+		  }});
+		}
+		//regular click, no ctrl key, dump the old array and create new one
+		//with only our clicked item of interest.
+		else{
+		  that.selectedNodes=[];
+		  that.selectedNodes.push(data);
+		  this.$scope.eventCallback({item:{
+			  eventType: "document-selected-single",
+			  document: data.valueItem
+		  }});
+		}
+	    
+    }
+    else{
+      this.$scope.eventCallback({item:data});
+    }
+
+    data.deferred.resolve();
+
+  };
+  
+  sdFolderTreeController.prototype.loadRootFolder = function(rootPath){
+    var that = this,
+        rootName,
+        root = {};
+        
+    rootName = rootPath.split("/");
+    root.uuid = "0001";
+    root.name = rootName[rootName.length-1];
+    root.nodeType="rootFolder";
+    root.isLoaded=true;
+    root.items=[];
+    this.folderService.getFolders(that.$scope.rootPath)
+    .then(function(data){
+      root.items =  (that.normalizeFolderData(data).items);
+      that.folderData.push(root);//.normalizeFolderData(data));
+    });
+  };
+  
+  /**
+   * Takes JSON as returned from our rest-common endpoint and normalizes
+   * it to a common structure which our tree driective can recurse over.
+   * Adds an itemType property to explicitly identify the node type.
+   * Adds a isLoaded property to keep track over whether the node has 
+   * loaded its children (on a previous expansion).
+   * @param folder
+   * @returns
+   */
+  sdFolderTreeController.prototype.normalizeFolderData = function(folder){
+    var normFolder = {items: []},
+        tempItem,
+        i=0;
+    
+    for(i=0;i<folder.documents.length;i++){
+      tempItem = folder.documents[i];
+      tempItem.itemType = "document";
+      normFolder.items.push(tempItem);
+    }
+    
+    for(i=0;i<folder.folders.length;i++){
+      tempItem = folder.folders[i];
+      tempItem.itemType = "folder";
+      tempItem.isLoaded = false;
+      normFolder.items.push(tempItem);
+    }
+    
+    return normFolder;
+    
+  };
+  
+  //Dependencies we will inject into the controller.
+  sdFolderTreeController.$inject = ["sdFolderService","$q","$scope"];
+  
+  mod.controller("sdFolderTreeController",sdFolderTreeController);
+  
+  mod.directive("sdFolderTree",function(){
+    return {
+      scope: {
+        rootPath : "@sdaRootPath",
+        allowMultiselect : "@sdaMultiselect",
+        onInit : "&sdaOnInit",
+        eventCallback: "&sdaEventCallback"
+      },
+      controller: "sdFolderTreeController",
+      controllerAs: "treeCtrl",
+      templateUrl : "./plugins/html5-common/scripts/directives/sdFolderTree/sdFolderTree.html"
+    };
+  });
+  
+})();
