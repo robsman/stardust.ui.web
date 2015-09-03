64f3c323779d825af1000a68794ba098bb4e757b
Yogesh Manware <yogesh.manware@sungard.com>
2015-09-03 14:48:16 +0530

    [CRNT-37987]: unit testing bug fix
    Bug: CRNT-37987

--- html5-views-common/src/main/resources/META-INF/webapp/html5/scripts/controllers/sdCorrespondenceViewCtrl.js
+++ html5-views-common/src/main/resources/META-INF/webapp/html5/scripts/controllers/sdCorrespondenceViewCtrl.js
@@ -2,10 +2,8 @@
  * Copyright (c) 2011 SunGard CSA LLC and others. All rights reserved. This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors: SunGard CSA LLC - initial API and implementation and/or initial
- * documentation
+ * available at http://www.eclipse.org/legal/epl-v10.html Contributors: SunGard
+ * CSA LLC - initial API and implementation and/or initial documentation
  ******************************************************************************/
 
 /**
@@ -13,100 +11,141 @@
  */
 
 (function() {
-	'use strict';
+  'use strict';
 
-	angular.module("viewscommon-ui").controller(
-			'sdCorrespondenceViewCtrl',
-			['$scope','$q','$parse','sdUtilService','sdFolderService', Controller]);
+  angular.module("viewscommon-ui").controller('sdCorrespondenceViewCtrl',
+          ['$scope', '$q', '$parse', 'sdUtilService', 'sdFolderService', Controller]);
 
-	var _parse = null;
-	var _sdFolderService = null;
+  var _parse = null;
+  var _sdFolderService = null;
 
-	/*
-	 * 
-	 */
-	function Controller( $scope, $q, $parse, sdUtilService, sdFolderService) {
-		this.readOnly = true;
-		
-		_parse = $parse;
-		_sdFolderService = sdFolderService;
-		
-		this.correspondenceTypes = [{
-			label : 'Email',
-			id : 'email'
-		}, {
-			label : 'Print',
-			id : 'print'
-		}];
-		
-		
-		this.documentParams = {
-				
-		}
-		
-		this.selected = {
-				
-		};
-		
-		
-		this.documentParams  = {
-				disableSaveAction : true
-		}
-		
-		this.populateCorrespondenceData($scope);
-	}
-	
-	
-	Controller.prototype.populateCorrespondenceData = function($scope){
-		var ctrl = this;
-		var queryGetter = _parse("panel.params.custom");
-		var params = queryGetter($scope);
+  /*
+   * 
+   */
+  function Controller($scope, $q, $parse, sdUtilService, sdFolderService) {
+    this.readOnly = true;
 
-		if(params && params.folderId) {
-			ctrl.folderId = params.folderId;
-			console.log(ctrl.folderId);
-			ctrl.getExistingFolderInformation(ctrl.folderId);
-		} else {
-			console.error("Couldnt Retrive Folder ID");
-		}
-	};
-	
-	/**
-	 * 
-	 */
-	Controller.prototype.getExistingFolderInformation = function( folderId ){
-		var ctrl = this;
-		_sdFolderService.getFolderInformationByFolderId(folderId).then(function(data){
-			console.log("Return from getExistingFolderInformation using folder id - "+folderId)
-			console.log(data);
-			ctrl.selected = populateCorrespondenceMetaData(data.correspondenceMetaDataDTO, data.documents )
-		});
-	}
-	
-	function populateCorrespondenceMetaData(metaData, documents){
-		var type = 'print'
-		if(metaData.to || metaData.bcc || metaData.cc) {
-			type = 'email'
-		}
-		
-		var uiData =  {
-			type  : type, // print / email
-			to: metaData.to,
-			bcc:metaData.bcc,
-			cc:metaData.cc,
-			content : metaData.content,
-			subject : metaData.subject,
-			templateId : '',
-			attachments : documents,
-			aiOid : '',
-			showBcc : metaData.bcc ? metaData.bcc.length > 0 : false,
-			showCc :   metaData.cc ? metaData.cc.length > 0 : false
-		}
-		console.log("Populated object")
-		console.log(uiData)
-		return uiData;
-	}
+    _parse = $parse;
+    _sdFolderService = sdFolderService;
+    this.correspondenceTypes = [{
+      label: 'Email',
+      id: 'email'
+    }, {
+      label: 'Print',
+      id: 'print'
+    }];
 
-	
+    this.documentParams = {
+
+    }
+
+    this.selected = {
+
+    };
+
+    this.documentParams = {
+      disableSaveAction: true
+    }
+
+    this.populateCorrespondenceData($scope);
+  }
+
+  Controller.prototype.populateCorrespondenceData = function($scope) {
+    var ctrl = this;
+    var queryGetter = _parse("panel.params.custom");
+    var params = queryGetter($scope);
+
+    if (params && params.folderId) {
+      ctrl.folderId = params.folderId;
+      console.log(ctrl.folderId);
+      ctrl.getExistingFolderInformation(ctrl.folderId);
+    } else {
+      console.error("Couldnt Retrive Folder ID");
+    }
+  };
+
+  Controller.prototype.addressIconMapper = function(item, index) {
+    var tagClass = "glyphicon glyphicon-envelope"
+
+    if (item.type == 'fax') {
+      tagClass = "fa fa-fax";
+    }
+    return tagClass;
+  };
+
+  /**
+   * 
+   */
+  Controller.prototype.getExistingFolderInformation = function(folderId) {
+    var ctrl = this;
+    _sdFolderService.getFolderInformationByFolderId(folderId).then(function(data) {
+      console.log("Return from getExistingFolderInformation using folder id - " + folderId)
+      console.log(data);
+      ctrl.selected = populateCorrespondenceMetaData(data.correspondenceMetaDataDTO, data.documents)
+    });
+  }
+
+  function populateCorrespondenceMetaData(metaData, documents) {
+    var type = "print";
+    if (isEmail(metaData)) {
+      type = "email"
+    }
+
+    var uiData = {
+      type: type, // print / email
+      to: formatInDataAddress(metaData.To),
+      BCC: metaData.BCC,
+      cc: metaData.CC,
+      content: metaData.MessageBody,
+      subject: metaData.Subject,
+      templateId: '',
+      attachments: formatInDataAttachments(metaData.Attachments),
+      aiOid: '',
+      showBCC: metaData.BCC ? metaData.BCC.length > 0 : false,
+      showCc: metaData.CC ? metaData.CC.length > 0 : false
+    }
+    console.log("Populated object")
+    console.log(uiData)
+    return uiData;
+  }
+
+  function formatInDataAddress(addresses) {
+    var outAddresses = [];
+    angular.forEach(addresses, function(data) {
+      if (data.Address && data.Address.length > 1) {
+        var type = "email";
+        if (data.IsFax) {
+          type = "fax";
+        }
+        outAddresses.push({
+          name: data.DataPath,
+          value: data.Address,
+          type: type
+        });
+      }
+    });
+
+    return outAddresses;
+  }
+
+  function formatInDataAttachments(attachments) {
+    var outAttachments = [];
+    angular.forEach(attachments, function(data) {
+      if (data.DocumentId && data.DocumentId.length > 1) {
+        outAttachments.push({
+          path: data.DocumentId,
+          uuid: data.DocumentId,
+          templateDocuemntId: data.TemplateDocumentId,
+          name: data.Name ? data.Name : "sample"
+        })
+      }
+    });
+    return outAttachments;
+  }
+
+  function isEmail(metaData) {
+    if (metaData.To || metaData.BCC || metaData.CC) { return true }
+    return false;
+  }
 
 })();
