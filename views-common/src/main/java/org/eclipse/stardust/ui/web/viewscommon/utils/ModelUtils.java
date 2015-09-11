46a08fe03822d1c57b54645e34294d661d2cf5d4
Rainer Pielmann <rainer.pielmann@sungard.com>
2015-09-11 13:40:05 +0200

    CRNT-35079 UI Mashup Application: Data Drag and Dropped from anothermodel throwing NPE at runtime.
    Signed-off-by: Rainer Pielmann <rainer.pielmann@sungard.com>

--- views-common/src/main/java/org/eclipse/stardust/ui/web/viewscommon/utils/ModelUtils.java
+++ views-common/src/main/java/org/eclipse/stardust/ui/web/viewscommon/utils/ModelUtils.java
@@ -408,8 +408,11 @@ public class ModelUtils
          {
             String parts[] = typeDeclarationId.split("\\{")[1].split("\\}");
             typeDeclarationId = parts[1];
-            Model newRefModel = getModel(model.getResolvedModelOid(parts[0]));
-            refModel = newRefModel != null ? newRefModel : refModel;
+            if (!parts[0].equals(refModel.getId()))
+            {
+               Model newRefModel = getModel(model.getResolvedModelOid(parts[0]));
+               refModel = newRefModel != null ? newRefModel : refModel;
+            }
          }
          catch (Exception e)
          {
