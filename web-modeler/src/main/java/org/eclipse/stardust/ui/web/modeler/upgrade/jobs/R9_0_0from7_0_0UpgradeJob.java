f8e067ddce030f9a4b2d226ed8b2b5c8386bcb5d
Barry Grotjahn <barry.grotjahn@sungard.com>
2015-09-25 10:34:27 +0200

    CRNT-38265 enhance upgrade job for moving connections

--- web-modeler/src/main/java/org/eclipse/stardust/ui/web/modeler/upgrade/jobs/R9_0_0from7_0_0UpgradeJob.java
+++ web-modeler/src/main/java/org/eclipse/stardust/ui/web/modeler/upgrade/jobs/R9_0_0from7_0_0UpgradeJob.java
@@ -11,15 +11,21 @@
 
 package org.eclipse.stardust.ui.web.modeler.upgrade.jobs;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.UUID;
 
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+import org.eclipse.stardust.common.CollectionUtils;
 import org.eclipse.stardust.common.config.Version;
 import org.eclipse.stardust.common.log.LogManager;
 import org.eclipse.stardust.common.log.Logger;
 import org.eclipse.stardust.model.xpdl.builder.defaults.DefaultElementsInitializer;
 import org.eclipse.stardust.model.xpdl.builder.spi.ModelInitializer;
-import org.eclipse.stardust.model.xpdl.carnot.ModelType;
+import org.eclipse.stardust.model.xpdl.carnot.*;
 import org.eclipse.stardust.model.xpdl.carnot.util.AttributeUtil;
+import org.eclipse.stardust.model.xpdl.carnot.util.DiagramUtil;
 import org.eclipse.stardust.ui.web.modeler.upgrade.UpgradeJob;
 
 public class R9_0_0from7_0_0UpgradeJob extends UpgradeJob
@@ -54,7 +60,49 @@ public class R9_0_0from7_0_0UpgradeJob extends UpgradeJob
       ModelInitializer initializer = new DefaultElementsInitializer();
       initializer.initializeModel(model);
       checkModelUUIDs(model);
+      checkConnections(model);
       return model;
+   }
+
+   private void checkConnections(ModelType model)
+   {
+      for(ProcessDefinitionType process : model.getProcessDefinition())
+      {
+         for(DiagramType diagram : process.getDiagram())
+         {
+            PoolSymbol defaultPool = DiagramUtil.getDefaultPool(diagram);
+            if(defaultPool != null)
+            {
+               List<IConnectionSymbol> elements = CollectionUtils.newArrayList();
+               for(LaneSymbol lane : defaultPool.getLanes())
+               {
+                  if(!lane.getConnections().isEmpty())
+                  {
+                     for (Iterator i = lane.getConnections().valueListIterator(); i.hasNext();)
+                     {
+                        IConnectionSymbol connection = (IConnectionSymbol) i.next();
+                        elements.add(connection);
+                     }                  
+                  }
+                  
+                  if(elements.size() > 0)
+                  {
+                     for(IConnectionSymbol element : elements)
+                     {
+                        EStructuralFeature feature = element.eContainingFeature();
+                        if(feature != null)
+                        {
+                           List list = (List) defaultPool.eGet(feature);
+                           list.add(element);                           
+                        }
+                     }
+                     
+                     lane.getConnections().removeAll(elements);
+                  }
+               }
+            }
+         }
+      }      
    }
 
    public void checkModelUUIDs(ModelType model)
