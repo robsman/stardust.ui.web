651a34c09a771048b43d1e540cdba8349ddedced
Barry Grotjahn <barry.grotjahn@sungard.com>
2015-09-29 14:55:00 +0200

    CRNT-38428 merge changes from CRNT-38264 (unit test)

---
+++ web-modeler/src/test/java/org/eclipse/stardust/ui/web/modeler/xsd/ClassesHelperTest.java
@@ -0,0 +1,32 @@
+package org.eclipse.stardust.ui.web.modeler.xsd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.eclipse.stardust.ui.web.modeler.xpdl.edit.utils.ClassesHelper;
+
+public class ClassesHelperTest
+{
+
+   @Test
+   public void testClassesHelper()
+   {
+      String methodSignature = "testSignature(java.lang.String, java.util.List<java.util.Map<java.lang.String,  java.util.Map<java.lang.String, java.lang.Object>>>, java.util.List<java.lang.String>, java.util.Map<java.lang.String, java.lang.Object>)";
+      
+      
+      ClassLoader classLoader = ClassesHelperTest.class.getClassLoader();
+      try
+      {
+         ClassesHelper.getMethodBySignature(classLoader, "org.eclipse.stardust.ui.web.modeler.xsd.ClassesHelperTest", methodSignature);
+      }
+      catch (Throwable e)
+      {
+         Assert.fail("ClassesHelper.getMethodBySignature failed!");
+      }
+   }
+   
+   public void testSignature(String a, java.util.List<java.util.Map<java.lang.String, java.util.Map<java.lang.String, java.lang.Object>>> b, java.util.List<java.lang.String> v, java.util.Map<java.lang.String, java.lang.Object> r)
+   {
+      
+   }
+}
