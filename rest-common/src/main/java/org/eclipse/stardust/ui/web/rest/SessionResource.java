eff1e9f139071044295f23c24d98ebe1e853962d
Sidharth Singh <sidharth.singh@sungard.com>
2015-10-06 17:44:15 +0530

    [CRNT-38450]: Create REST end-point to invalidate (current) user session.
    Renamed HeartBeatResource to SessionResource
    Changed rest endpoint for "/ping" to "/session/ping"
    Added REST endpoint /session/invalidate
    
    Bug:CRNT-38450

--- rest-common/src/main/java/org/eclipse/stardust/ui/web/rest/SessionResource.java
+++ rest-common/src/main/java/org/eclipse/stardust/ui/web/rest/SessionResource.java
@@ -10,10 +10,14 @@
  *******************************************************************************/
 package org.eclipse.stardust.ui.web.rest;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -21,13 +25,26 @@ import org.springframework.stereotype.Component;
  * @version $Revision: $
  */
 @Component
-@Path("/ping")
-public class HeartBeatResource
+@Path("/session")
+public class SessionResource
 {
-
+   
+   @Autowired
+   private HttpServletRequest httpRequest;
+   
    @GET
+   @Path("/ping")
    public Response getActivityInstance()
    {
+      return Response.ok().build();
+   }
+   
+   @DELETE
+   @Path("/invalidate")
+   public Response invalidate()
+   {
+      HttpSession session = httpRequest.getSession();
+      session.invalidate();
       return Response.ok().build();
    }
 }
