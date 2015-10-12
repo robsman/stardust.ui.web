ce29b51de749e9480a88e38be4790bc19b4eddde
Aditya Gaikwad <aditya.gaikwad@sungard.com>
2015-10-12 18:22:43 +0530

    [CRNT-37996]: Miscellaneous fixes for image replacement
    Bug: CRNT-37996

--- html5-business-control-center/src/main/resources/META-INF/webapp/scripts/directives/sdRoleAssignmentColumnContent.js
+++ html5-business-control-center/src/main/resources/META-INF/webapp/scripts/directives/sdRoleAssignmentColumnContent.js
@@ -26,7 +26,7 @@
 				value : '=sdaValue'
 			},
 
-			template : '<div><i ng-if="ctrl.value == true" class="pi pi-lg pi-check"><\/i>'
+			template : '<div style="text-align: center"><i ng-if="ctrl.value == true" class="pi pi-lg pi-check"><\/i>'
 					+ '<i ng-if="ctrl.value == false" class="pi p-lg pi-close pi-danger"><\/i>'
 					+ '<\/div>',
 			controller : [ '$scope', '$parse', '$attrs', RoleAssignmentColumnContentCtrl ]
