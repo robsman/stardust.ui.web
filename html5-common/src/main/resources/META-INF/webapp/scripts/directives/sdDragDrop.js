fbbcbe7e2710e9a0b39d6aa4915420b8ed70b9fd
Johnson Quadras <johnson.quadras@sungard.com>
2015-12-10 16:09:35 +0530

    [CRNT-39170] : Fix scrolling issue with IE11. Adding check to see if sdDataDrag and sdDataDrop were used on the same element.
    Signed-off-by: Johnson Quadras <johnson.quadras@sungard.com>

--- html5-common/src/main/resources/META-INF/webapp/scripts/directives/sdDragDrop.js
+++ html5-common/src/main/resources/META-INF/webapp/scripts/directives/sdDragDrop.js
@@ -9,25 +9,25 @@
  *     SunGard CSA LLC - initial API and implementation and/or initial documentation
  *******************************************************************************/
 (function(){
-	
+
 	//Sniff browser so we can set the proper dataType on the dataTransfer API
 	var dataType = "text/plain";
-	
+
 	if(navigator.appName==="Microsoft Internet Explorer"){
 		dataType = "text";
 	}
-	
+
 	/**
 	 * sdDataDrop: makes the associated element a drop target with
 	 * a user defined callback for drop events - attrs.onDrop
 	 */
 	angular.module('bpm-common.directives')
 	.directive("sdDataDrop",function($parse){
-		
+
 	   return{
 	    scope: true,
 	    link: function(scope, elem, attrs){
-	      
+
 	      elem.attr("droppable","true");
 
 	      elem.bind("dragenter",function(e){
@@ -35,11 +35,11 @@
 	        elem.addClass("drop-over");
 	        return true;
 	      });
-	      
+
 	      elem.bind("dragleave",function(e){
-	    	  elem.removeClass("drop-over");  
+	    	  elem.removeClass("drop-over");
 	      });
-	      
+
 	      elem.bind("dragover",function(e){
 	          if (e.preventDefault) {e.preventDefault();}
 	          if (e.stopPropagation) {e.stopPropagation();}
@@ -47,11 +47,14 @@
 	          e.dataTransfer.dropEffect = 'move';
 	          return false;
 	      });
-	      
+
+	     
 	      elem.bind("drag",function(){
-	        return false;
+	    	 if (attrs.sdDataDrag === undefined) {
+	    		 return false;
+	    	 }
 	      });
-	      
+
 	      elem.bind("drop",function(e){
 	    	if (e.preventDefault) {e.preventDefault();}
 	        if (e.stopPropagation) {e.stopPropagation();}
@@ -64,66 +67,66 @@
 	    }
 	  };
 	});//sd-data-drop end
-	
-	
+
+
 	/**
 	 * sdDataDrag: makes the associated element draggable with
 	 * a user defined callback for drop events - attrs.onDrop
 	 */
 	angular.module('bpm-common.directives')
 	.directive("sdDataDrag",function($parse){
-		
-		
+
+
 	  return{
 	    require: "ngModel",
 	    link: function(scope, elem, attrs,ngModelCtrl){
-	      
+
 	      var data=ngModelCtrl.$viewValue,
     	      dragElem,
     	      cloneElem;
-	      
+
 	      elem.attr("draggable","true");
 
 	      elem.bind("dragend",function(e){
 	        var fn = $parse(attrs.sdaDragend);
-	        
+
 	        srcObject=ngModelCtrl.$modelValue;
 	        fn(scope, {$data: srcObject, $event: e});
-	        
+
 	        if(dragElem && dragElem.remove){
 	          dragElem.remove();
 	        }
-	        
+
 	        if(cloneElem && cloneElem.remove){
 	          cloneElem.remove();
 	        }
-	        
+
 	      });
 
 	      elem.bind("dragstart",function(e){
-	    	  
+
 	        var data =ngModelCtrl.$viewValue,
 	        	  fn = $parse(attrs.sdaDragstart),
 	            fn2 = $parse(attrs.sdaDragElemFx),
 	            dragObj;
 
-	          
+
 	        //DragStart callback
 	        data = fn(scope, {$data: ngModelCtrl.$viewValue, $event: e}) || data;
-	        
+
 	        //Drag element factory callback
 	        dragString =  fn2(scope, {item: ngModelCtrl.$viewValue});
-	        
-            
+
+
 	        if(dragString){
   	        //now create drag element and append to top of body
   	        dragElem = $(dragString);
   	        dragElem.css({"position":"absolute","top":0,"left":0,"z-index" : -2});
-  	        
-  	        
+
+
   	        //now createClone
   	        cloneElem = dragElem.clone();
-  	        
+
   	        //cloneElem.css(cloneCss);
   	        //cloneElem.children().css(cloneCss);
   	        cloneElem.addClass("sd-clone-elem");
@@ -132,21 +135,23 @@
 	        }
 	        //ensure we at least pass an empty string
 	        if(angular.isUndefined(data)){data="";}
-	        
+
 	        data = JSON.stringify(data);
-	        
+
 	        e = (e.dataTransfer)?e: e.originalEvent;
 	        e.dataTransfer.setData(dataType,data);
 	        e.dataTransfer.dropEffect = 'move';
 	        if(e.dataTransfer.setDragImage){
-	        	e.dataTransfer.setDragImage(dragElem[0],0,0);
+	        	if(dragElem) {
+	        		e.dataTransfer.setDragImage(dragElem[0],0,0);
+	        	}
 	        }
 	        e.dataTransfer.effectAllowed="move";
 
 	      });
-	      
+
 	    }
 	  };
 	});//sd-data-drag end
-	
+
 })();
