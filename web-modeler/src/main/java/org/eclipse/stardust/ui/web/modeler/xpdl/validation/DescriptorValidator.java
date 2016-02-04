928f959a069cf7412be1587d9e928be054d32307
Rainer Pielmann <rainer.pielmann@sungard.com>
2016-02-04 21:59:04 +0100

    CRNT-39502 Modelvalidation / Deploymentvalidation
    Signed-off-by: Rainer Pielmann <rainer.pielmann@sungard.com>

--- web-modeler/src/main/java/org/eclipse/stardust/ui/web/modeler/xpdl/validation/DescriptorValidator.java
+++ web-modeler/src/main/java/org/eclipse/stardust/ui/web/modeler/xpdl/validation/DescriptorValidator.java
@@ -11,15 +11,14 @@
 package org.eclipse.stardust.ui.web.modeler.xpdl.validation;
 
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.eclipse.emf.ecore.xml.type.internal.RegEx;
-
-import org.eclipse.stardust.model.xpdl.carnot.*;
+import org.eclipse.stardust.model.xpdl.carnot.AttributeType;
+import org.eclipse.stardust.model.xpdl.carnot.DataPathType;
+import org.eclipse.stardust.model.xpdl.carnot.IModelElement;
+import org.eclipse.stardust.model.xpdl.carnot.ProcessDefinitionType;
 import org.eclipse.stardust.model.xpdl.carnot.util.AttributeUtil;
 import org.eclipse.stardust.model.xpdl.carnot.util.VariableContextHelper;
 import org.eclipse.stardust.modeling.validation.*;
@@ -27,6 +26,7 @@ import org.eclipse.stardust.modeling.validation.*;
 public class DescriptorValidator implements IModelElementValidator
 {
    private Pattern pattern = Pattern.compile("(\\%\\{[^{}]+\\})"); //$NON-NLS-1$
+   private Map<String, DataPathReference> refMap = null;
    
    public Issue[] validate(IModelElement element) throws ValidationException
    {
@@ -38,64 +38,50 @@ public class DescriptorValidator implements IModelElementValidator
          AttributeType attribute = AttributeUtil.getAttribute(dataPathType, "type");
          if (attribute != null)
          {
-            String value = dataPathType.getDataPath();
-            value = VariableContextHelper.getInstance().getContext(element)
-                  .replaceAllVariablesByDefaultValue(value);
-            validateDescriptor(value, dataPathType, issues);
+            refMap = new HashMap<String, DataPathReference>(); 
+            DataPathReference reference = new DataPathReference(dataPathType, new ArrayList<DataPathReference>());
+            refMap.put(dataPathType.getId(), reference);
+            resolveReferences(reference, issues);
          }
       }
       return (Issue[]) issues.toArray(Issue.ISSUE_ARRAY);
    }
-
    
-   private String validateDescriptor(String value, DataPathType dataPathType, List<Issue> issues)
-   {
-      
-      if (value == null)
-      {         
-         issues.add(Issue.warning(dataPathType,
-               MessageFormat.format(      
-                     Validation_Messages.WR_COMPOSITE_LINK_DESCRIPTOR_NO_DATAPATH,                     
-                     new Object[] {dataPathType.getId()}),
-               ValidationService.PKG_CWM.getProcessDefinitionType_DataPath()));
-         return null;
+   private void resolveReferences(DataPathReference reference, List<Issue> issues)
+   { 
+      DataPathType dataPathType = reference.getDataPath();
+      String value = VariableContextHelper.getInstance().getContext(dataPathType).replaceAllVariablesByDefaultValue(dataPathType.getDataPath());
+      if (!this.hasVariabled(value))
+      {
+         return;
       }
-  
       String id = null;
-      String newValue = value;
-      Matcher matcher = pattern.matcher(newValue);
+      Matcher matcher = pattern.matcher(value);
       while (matcher.find())
       {
          if ((matcher.start() == 0) || ((matcher.start() > 0)
-               && (newValue.charAt(matcher.start() - 1) != '\\')))
+               && (value.charAt(matcher.start() - 1) != '\\')))
          {
-            String ref = newValue.substring(matcher.start(), matcher.end());
+            String ref = value.substring(matcher.start(), matcher.end());
             ref = ref.trim();
             id = ref;
             id = id.replace("%{", "");
             id = id.replace("}", "");
-            if (id.equals(dataPathType.getId()))
-            {
-               issues.add(Issue.error(dataPathType,
-                     MessageFormat.format(
-                           Validation_Messages.ERR_REFERENCED_DATAPTH_IS_A_CIRCULAR_DEPENDENCY,                           
-                           new Object[] {dataPathType.getId()}),
-                     ValidationService.PKG_CWM.getProcessDefinitionType_DataPath()));
-               return value;
-            }
-
             ProcessDefinitionType process = (ProcessDefinitionType) dataPathType.eContainer();
-            DataPathType refDataPath = findDataPath(process, id);
-            if (refDataPath == null)
+            DataPathType refDataPathType = findDataPath(process, id); 
+            
+            
+            if (refDataPathType == null)
             {
                issues.add(Issue.error(dataPathType,
                      MessageFormat.format(
                            Validation_Messages.ERR_REFERENCED_DESCRIPTOR_DOES_NOT_EXIST,                           
                            new Object[] {dataPathType.getId(), ref}),
                      ValidationService.PKG_CWM.getProcessDefinitionType_DataPath()));
-               return value;
+               return;
             }
-            String refAccessPath = refDataPath.getDataPath();
+            String refAccessPath = refDataPathType.getDataPath();
+            
             if (refAccessPath == null)
             {
                issues.add(Issue.warning(dataPathType,
@@ -103,25 +89,56 @@ public class DescriptorValidator implements IModelElementValidator
                            Validation_Messages.WR_REFERENCED_DESCRIPTOR_NO_DATAPATH,                           
                            new Object[] {ref}),
                      ValidationService.PKG_CWM.getProcessDefinitionType_DataPath()));
-               refAccessPath = "";
             }
-            value = this.replaceLiteral("%{" + id + "}", value, refAccessPath);
-         }
-         else
-         {
-            if (newValue.charAt(matcher.start() - 1) == '\\')
+                                                     
+            DataPathReference refDataPathTypeReference = refMap.get(refDataPathType.getId());
+            if (refDataPathTypeReference == null)
             {
-               value = value.replaceFirst("\\\\\\%\\{", "*0*0*0*0*");
+               refDataPathTypeReference = new DataPathReference(refDataPathType,
+                     new ArrayList<DataPathReference>());
+               reference.getReferences().add(refDataPathTypeReference);
+               refMap.put(refDataPathType.getId(), refDataPathTypeReference);
+            }     
+            if (this.hasCircularDependency(dataPathType.getId(), refDataPathTypeReference)) 
+            {
+               issues.add(Issue.error(dataPathType,
+                     MessageFormat.format(
+                           Validation_Messages.ERR_REFERENCED_DATAPTH_IS_A_CIRCULAR_DEPENDENCY,                           
+                           new Object[] {dataPathType.getId()}),
+                     ValidationService.PKG_CWM.getProcessDefinitionType_DataPath()));
+               return;
+            }
+            resolveReferences(refDataPathTypeReference, issues);            
+         }
+      }
+      return;
+   }
+   
+   private boolean hasCircularDependency(String referencingDataPathID,
+         DataPathReference referencedDataPath)
+   {
+      if (referencingDataPathID
+            .equalsIgnoreCase(referencedDataPath.getDataPath().getId()))
+      {
+         return true;
+      }
+      List<DataPathReference> references = referencedDataPath.getReferences();
+      for (Iterator<DataPathReference> i = references.iterator(); i.hasNext();)
+      {
+         DataPathReference reference = i.next();
+         if (hasCircularDependency(referencedDataPath.getDataPath().getId(), reference))
+         {
+            return true;
+         } else {
+            if (this.hasCircularDependency(referencingDataPathID, reference)) 
+            {
+               return true;
             }
          }
       }
-      if (value.indexOf("%") > -1)
-      {
-         return validateDescriptor(value, dataPathType, issues);
-      }      
-      return value;
+      return false;
    }
-   
+      
    private DataPathType findDataPath(ProcessDefinitionType process, String ref)
    {
       for (Iterator<DataPathType> i = process.getDataPath().iterator(); i.hasNext();)
@@ -134,52 +151,41 @@ public class DescriptorValidator implements IModelElementValidator
       }
       return null;
    }
+      
+   private boolean hasVariabled(String value)
+   {
+      Matcher matcher = pattern.matcher(value);
+      return matcher.find();
+   }
    
-   private String replaceLiteral(String literal, String value, String newValue)
-   {      
-      String tobeReplaced = "";
-      String replacement = "";
-      if (!newValue.startsWith("%{")) //$NON-NLS-1$
+   public class DataPathReference
+   {
+      private DataPathType dataPath;
+      private List<DataPathReference> references = new ArrayList<DataPathReference>();
+      
+      public DataPathType getDataPath()
       {
-         tobeReplaced = literal.substring(2, literal.length() - 1);
-         replacement = newValue;
-         if (replacement.indexOf("%") > -1) //$NON-NLS-1$
-         {
-            replacement = replacement.replace("%", "\\%"); //$NON-NLS-1$ //$NON-NLS-2$
-         }
-         List<String> list1 = new ArrayList<String>();
-         while (value.indexOf("%{" + tobeReplaced + "}") > -1) //$NON-NLS-1$ //$NON-NLS-2$
-         {
-            int idx = value.indexOf("%{" + tobeReplaced + "}"); //$NON-NLS-1$ //$NON-NLS-2$
-            if (idx == 0 || (idx > 0 && value.charAt(idx - 1) != '\\'))
-            {
-               value = value.replaceFirst("(\\%\\{" + tobeReplaced + "\\})", replacement); //$NON-NLS-1$ //$NON-NLS-2$
-            }
-            else
-            {
-               list1.add("\\%\\{" + tobeReplaced + "\\}"); //$NON-NLS-1$ //$NON-NLS-2$
-               value = value.replaceFirst("(\\%\\{" + tobeReplaced + "\\})", "*0*0*0*0*"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-            }
-         }
-         for (Iterator<String> i = list1.iterator(); i.hasNext();)
-         {
-            String string = i.next();
-            value = value.replaceFirst("(\\*0\\*0\\*0\\*0\\*)", string); //$NON-NLS-1$
-         }
+         return dataPath;
       }
-      else
+      public void setDataPath(DataPathType dataPath)
       {
-         tobeReplaced = literal.substring(2, literal.length() - 1);
-         replacement = newValue.substring(2, newValue.length() - 1);
-         if (replacement.indexOf("%") > -1) //$NON-NLS-1$
-         {
-            replacement = replacement.replace("%", "\\%"); //$NON-NLS-1$ //$NON-NLS-2$
-         }
-         tobeReplaced = RegEx.REUtil.quoteMeta(tobeReplaced);
-         value = value.replaceAll("(\\%\\{" + tobeReplaced + "\\})", "\\%\\{" //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-               + replacement + "\\}"); //$NON-NLS-1$
+         this.dataPath = dataPath;
       }
-      return value;
+      public List<DataPathReference> getReferences()
+      {
+         return references;
+      }
+      public void setReferences(List<DataPathReference> references)
+      {
+         this.references = references;
+      }
+      public DataPathReference(DataPathType dataPath, List<DataPathReference> references)
+      {
+         super();
+         this.dataPath = dataPath;
+         this.references = references;
+      }
+
    }
 
 }
