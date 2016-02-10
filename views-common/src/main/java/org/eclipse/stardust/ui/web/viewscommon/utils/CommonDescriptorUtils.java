6d66c8b77c6b4ba34ab313632e6f45092b5ec3e5
Sidharth Singh <sidharth.singh@sungard.com>
2016-02-10 11:50:34 +0530

    [CRNT-39562]: Business Object Start: Descriptor values do not display for Processes started from Business Object Start
    Merged the changes made at CRNT-39518 for cross-model descriptors
    
    Bug: CRNT-39562

--- views-common/src/main/java/org/eclipse/stardust/ui/web/viewscommon/utils/CommonDescriptorUtils.java
+++ views-common/src/main/java/org/eclipse/stardust/ui/web/viewscommon/utils/CommonDescriptorUtils.java
@@ -333,43 +333,75 @@ public static List<ProcessDescriptor> createProcessDescriptors(Map<String, Objec
                }
                else
                {
-                  // Format numeric descriptor values with thousand's seperator if supported by
-                  // underlying data
-                  if (dataPathDetails.isDescriptor())
+                  try
                   {
-                     if (!isEmpty(descriptorValue))
+                     // Format numeric descriptor values with thousand's seperator if
+                     // supported by
+                     // underlying data
+                     if (dataPathDetails.isDescriptor())
                      {
-                        if (null != dataDetails && StructuredDataConstants.STRUCTURED_DATA.equals(dataDetails.getTypeId()))
+                        Model refModel = model;
+                        Data data = null;
+                        if (!isEmpty(descriptorValue))
                         {
-                           Class dataClass = dataPathDetails.getMappedType();
-                           IXPathMap xPathMap = ClientXPathMap.getXpathMap(model, dataDetails);
-                           String xPath = StructuredDataXPathUtils.getXPathWithoutIndexes(dataPathDetails.getAccessPath());
-                           TypedXPath typedXPath = xPathMap.getXPath(xPath);
-                           if(null != typedXPath)
+                           if (null != dataDetails
+                                 && StructuredDataConstants.STRUCTURED_DATA.equals(dataDetails.getTypeId()))
                            {
-                              Number value = getNumericValue(dataClass, descriptorValue);
-                              if(value == null && "decimal".equals(typedXPath.getXsdTypeName()))
+                              Reference ref = dataDetails.getReference();
+                              if (null == ref)
                               {
-                                 value = new BigDecimal(descriptorValue.toString());
+                                 data = model.getData(dataPathDetails.getData());
+                                 ref = data.getReference();
                               }
-                              if(value !=null)
+                              if (null != ref)
                               {
-                                 XPathAnnotations annotation = typedXPath.getAnnotations();
-                                 if (annotation != null)
+                                 if (ref.getModelOid() != model.getModelOID())
                                  {
-                                    String descriptorLabelValue = annotation.getElementValue(
-                                          XPathAnnotations.IPP_ANNOTATIONS_NAMESPACE, new String[] {
-                                                "ui", Constants.INPUT_PREFERENCES_NUMBER_GROUP_KEY_LABEL});
-                                    if (null != descriptorLabelValue && Boolean.valueOf(descriptorLabelValue))
+                                    refModel = ModelCache.findModelCache().getModel(ref.getModelOid());
+                                 }
+                              }
+                              else
+                              {
+                                 if (data.getModelOID() != model.getModelOID())
+                                 {
+                                    refModel = ModelCache.findModelCache().getModel(data.getModelOID());
+                                 }
+                              }
+                              Class dataClass = dataPathDetails.getMappedType();
+                              IXPathMap xPathMap = ClientXPathMap.getXpathMap(refModel, dataDetails);
+                              String xPath = StructuredDataXPathUtils
+                                    .getXPathWithoutIndexes(dataPathDetails.getAccessPath());
+                              TypedXPath typedXPath = xPathMap.getXPath(xPath);
+                              if (null != typedXPath)
+                              {
+                                 Number value = getNumericValue(dataClass, descriptorValue);
+                                 if (value == null && "decimal".equals(typedXPath.getXsdTypeName()))
+                                 {
+                                    value = new BigDecimal(descriptorValue.toString());
+                                 }
+                                 if (value != null)
+                                 {
+                                    XPathAnnotations annotation = typedXPath.getAnnotations();
+                                    if (annotation != null)
                                     {
-                                       String numberValue = formatNumberInLocale(value);
-                                       descriptors.put(entry.getKey(), numberValue);
+                                       String descriptorLabelValue = annotation.getElementValue(
+                                             XPathAnnotations.IPP_ANNOTATIONS_NAMESPACE,
+                                             new String[] {"ui", Constants.INPUT_PREFERENCES_NUMBER_GROUP_KEY_LABEL});
+                                       if (null != descriptorLabelValue && Boolean.valueOf(descriptorLabelValue))
+                                       {
+                                          String numberValue = formatNumberInLocale(value);
+                                          descriptors.put(entry.getKey(), numberValue);
+                                       }
                                     }
                                  }
                               }
                            }
                         }
                      }
+                  }
+                  catch (Exception e)
+                  {
+                     trace.error("Error occured while adding Thousands Seperator", e);
                   }
                   GenericDataMapping mapping = new GenericDataMapping(dataPathDetails);
                   DataMappingWrapper dmWrapper = new DataMappingWrapper(mapping, null, false);
