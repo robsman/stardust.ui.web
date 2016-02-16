d07052e29f0bf06396c77bfe78937c2097852a36
McCain, Zach <zachary.mccain@sungard.com>
2016-02-16 08:53:54 -0600

    [CRNT-37163]: Workflow Execution > My Documents View: HTML5 Implementation.
    -Updated paths to use the new hash mapping capability of the repository directive. Added i18n translation for mapped path labels. Imported the sdReportsService so as to leverage its existing getCurrentUser function.
    
    Bug: CRNT-37163

--- html5-views-common/src/main/resources/META-INF/webapp/html5/scripts/controllers/sdMyDocumentsTreeViewCtrl.js
+++ html5-views-common/src/main/resources/META-INF/webapp/html5/scripts/controllers/sdMyDocumentsTreeViewCtrl.js
@@ -3,12 +3,41 @@
 	'use strict';
 	
 	//inject our dependencies for the controller
-	sdMyDocumentsTreeViewCtrl.$inject = [];
+	sdMyDocumentsTreeViewCtrl.$inject = ["sdReportsService", "sdI18nService"];
 	
-	function sdMyDocumentsTreeViewCtrl(){
-		this.path="/documents,/realms";
+	/**
+	 * [sdMyDocumentsTreeViewCtrl description]
+	 * @param  {[type]} sdReportsService [description]
+	 * @param  {[type]} sdI18nService    [description]
+	 * @return {[type]}                  [description]
+	 */
+	function sdMyDocumentsTreeViewCtrl(sdReportsService, sdI18nService){
+
+		var that = this;
+		var i18n = sdI18nService.getInstance('views-common-messages');
+
+		sdReportsService.getCurrentUser()
+		.then(function(user){
+			var paths = {};
+			paths["/documents"] = i18n.translate("views.myDocumentsTreeView.documentTree.commonDocumentsFolderLabel");
+			paths[user.myDocumentsFolderPath] = i18n.translate("views.myDocumentsTreeView.documentTree.myDocuments");
+			that.path = paths;
+		})
+		["catch"](function(err){
+			//TODO:err handling
+			alert("error");
+		});
+
+		//this.path="/documents,/realms";
 	}
 
+	/**
+	 * Hook function to intercept the menu creation callback of the document repository
+	 * directive.
+	 * @param  {[type]} menuItems [description]
+	 * @param  {[type]} treeNode  [description]
+	 * @return {[type]}           [description]
+	 */
 	sdMyDocumentsTreeViewCtrl.prototype.menuHook = function(menuItems,treeNode){
 
 		//Root folders may not be renamed or deleted so remove those items.
