d5eb59764d7777d7293ea438f92da30a13cc0b44
McCain, Zach <zachary.mccain@sungard.com>
2016-04-21 12:59:14 -0500

    [CRNT-37173]: Document Triage->BOI table style should be uniform
    - Removed active references to jquery.datatables.css as they were over-riding our data table styles in certain cases. Also, added a rule to the base data table css to keep the line height for our th and td elements standard.
    
    Bug: CRNT-37173

--- html5-process-portal/src/main/resources/META-INF/webapp/standalone/html5Deps.js
+++ html5-process-portal/src/main/resources/META-INF/webapp/standalone/html5Deps.js
@@ -16,7 +16,6 @@ var html5Deps = function() {
         "html5-common/styles/3rd-party/jquery-plugins/jquery-ui/1.10.2/jquery-ui-custom.css",
         "html5-common/styles/3rd-party/glyphicons/3.3.2/glyphicons.css",
         "html5-common/styles/portal-icons.css",
-        "html5-common/styles/3rd-party/datatables/1.9.4/jquery.dataTables.css",
         "html5-common/styles/3rd-party/bootstrap/3.3.2/css/bootstrap-theme.min.css",
         "html5-common/styles/3rd-party/bootstrap/3.3.2/css/bootstrap-modal.css",
         "html5-common/styles/sd-data-table.css",
