ff91850e9ceec2b359a6321c5499405972e0b931
Johnson Quadras <johnson.quadras@sungard.com>
2016-05-18 14:22:04 +0530

    [CRNT-40205] : Removing methods making sync calls. Bug :CRNT-40205
    Signed-off-by: Johnson Quadras <johnson.quadras@sungard.com>

--- html5-common/src/main/resources/META-INF/webapp/scripts/services/sdUtilService.js
+++ html5-common/src/main/resources/META-INF/webapp/scripts/services/sdUtilService.js
@@ -182,68 +182,7 @@
 				throw msg;
 			}
 		};
-
-		/*
-		 * For async, use angular resource
-		 */
-		UtilService.prototype.syncAjax = function(endpoint) {
-			var data, failed;
-
-			jQuery.ajax({
-				type : 'GET',
-				url : endpoint,
-				async : false,
-				contentType : 'application/json',
-				success : function(result) {
-					data = result;
-				},
-				error : function(errObj) {
-					failed = true;
-				}
-			});
-
-			if (failed) {
-				throw 'Error in invoking syncAjaxPost()';
-			}
-
-			return data;
-		};
-
-		/*
-		 * For async, use angular resource
-		 */
-		UtilService.prototype.syncAjaxSubmit = function(endpoint, value, type) {
-			var data, failed;
-
-			if (value != undefined && value != null) {
-				if (angular.isObject(value) || angular.isArray(value)) {
-					value = angular.toJson(value);
-				} else {
-					value = '' + value;
-				}
-			}
-
-			jQuery.ajax({
-				type : type ? type : 'POST',
-				url : endpoint,
-				async : false,
-				contentType : 'application/json',
-				data : value,
-				success : function(result) {
-					data = result;
-				},
-				error : function(errObj) {
-					failed = true;
-				}
-			});
-
-			if (failed) {
-				throw 'Error in invoking syncAjaxSubmit()';
-			}
-
-			return data;
-		};
-
+		
 		/**
 		 * 
 		 */
