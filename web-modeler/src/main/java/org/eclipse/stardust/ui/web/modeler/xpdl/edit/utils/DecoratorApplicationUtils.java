1578cb488f7dd12e9bb1412b18399fa2b084f31a
fradj.zayen <fradj.zayen@sungard.com>
2016-06-27 14:54:14 +0100

    [CRNT-40836]: Browser Modeler: Decorator Parameters are not storing numbers
    Bug: CRNT-40836
    Signed-off-by: fradj.zayen <fradj.zayen@sungard.com>

--- web-modeler/src/main/java/org/eclipse/stardust/ui/web/modeler/xpdl/edit/utils/DecoratorApplicationUtils.java
+++ web-modeler/src/main/java/org/eclipse/stardust/ui/web/modeler/xpdl/edit/utils/DecoratorApplicationUtils.java
@@ -9,6 +9,7 @@ import org.eclipse.stardust.model.xpdl.builder.utils.ModelBuilderFacade;
 import org.eclipse.stardust.model.xpdl.carnot.ApplicationType;
 import org.eclipse.stardust.model.xpdl.carnot.ApplicationTypeType;
 import org.eclipse.stardust.model.xpdl.carnot.ModelType;
+import org.eclipse.stardust.model.xpdl.carnot.ProcessDefinitionType;
 import org.eclipse.stardust.model.xpdl.carnot.util.AttributeUtil;
 import org.eclipse.stardust.model.xpdl.carnot.util.ModelUtils;
 
@@ -29,21 +30,28 @@ public class DecoratorApplicationUtils
             "stardust:application::decorator::elementType");
       String modelId = AttributeUtil.getAttributeValue(application,
             "stardust:application::decorator::modelId");
-      if (StringUtils.isNotEmpty(elementId) && StringUtils.isNotEmpty(elementType)
-            && StringUtils.isNotEmpty(modelId)
+      ModelType consumerModel = ModelUtils.findContainingModel(application);
+      int beginIndex = modelId.length() + 1;
+      ModelType providerModel =null;
+      if (StringUtils.isNotEmpty(elementType) && elementType.equalsIgnoreCase("application")
             )
       {
-         ModelType consumerModel = ModelUtils.findContainingModel(application);
-         int beginIndex = modelId.length() + 1;
          ApplicationType decoratedApplication = modelBuilderFacade.getApplication(modelId,
                elementId.substring(beginIndex));
-         ModelType providerModel = ModelUtils.findContainingModel(decoratedApplication);
-         List<ModelType> models = new ArrayList<ModelType>();
-         models.add(providerModel);
-         if (!ExternalReferenceUtils.isModelReferenced(consumerModel, models))
-         {
-            ExternalReferenceUtils.updateReferences(consumerModel, providerModel);
-         }
+            providerModel = ModelUtils.findContainingModel(decoratedApplication);
+           
+      }else if (StringUtils.isNotEmpty(elementType) && elementType.equalsIgnoreCase("process"))
+      {
+         ProcessDefinitionType decoratedProcess = modelBuilderFacade.getProcessDefinition(modelId,
+               elementId.substring(beginIndex));
+            providerModel = ModelUtils.findContainingModel(decoratedProcess);
+      }
+      
+      List<ModelType> models = new ArrayList<ModelType>();
+      models.add(providerModel);
+      if (!ExternalReferenceUtils.isModelReferenced(consumerModel, models))
+      {
+         ExternalReferenceUtils.updateReferences(consumerModel, providerModel);
       }
    }
 }
