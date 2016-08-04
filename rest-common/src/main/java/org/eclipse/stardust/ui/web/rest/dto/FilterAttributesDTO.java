7b1ecfa4ce4a1dcefebab7c369672bea4d41978f
Johnson Quadras <johnson.quadras@sungard.com>
2016-08-04 12:25:34 +0530

    [CRNT-41031] :  Removing unused methods from filterAttribuetesDTO Bug :CRNT-41031

--- rest-common/src/main/java/org/eclipse/stardust/ui/web/rest/dto/FilterAttributesDTO.java
+++ rest-common/src/main/java/org/eclipse/stardust/ui/web/rest/dto/FilterAttributesDTO.java
@@ -14,23 +14,8 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.TimeZone;
 
-import org.eclipse.stardust.engine.api.model.PredefinedConstants;
-import org.eclipse.stardust.engine.api.model.ProcessDefinition;
-import org.eclipse.stardust.engine.api.query.ActivityFilter;
-import org.eclipse.stardust.engine.api.query.ActivityInstanceQuery;
-import org.eclipse.stardust.engine.api.query.FilterAndTerm;
-import org.eclipse.stardust.engine.api.query.FilterTerm;
-import org.eclipse.stardust.engine.api.query.PerformingUserFilter;
-import org.eclipse.stardust.engine.api.query.ProcessDefinitionFilter;
-import org.eclipse.stardust.engine.api.query.ProcessInstanceHierarchyFilter;
-import org.eclipse.stardust.engine.api.query.ProcessInstanceQuery;
-import org.eclipse.stardust.engine.api.query.Query;
-import org.eclipse.stardust.engine.api.runtime.ActivityInstanceState;
-import org.eclipse.stardust.engine.api.runtime.ProcessInstanceState;
 import org.eclipse.stardust.engine.api.runtime.User;
-import org.eclipse.stardust.ui.web.bcc.ProcessSearchProvider;
 import org.eclipse.stardust.ui.web.rest.component.util.ProcessActivityUtils;
-import org.eclipse.stardust.ui.web.viewscommon.utils.ModelCache;
 
 /**
  * @author Aditya.Gaikwad
@@ -66,145 +51,7 @@ public class FilterAttributesDTO implements Serializable
       priority = ProcessActivityUtils.ALL_PRIORITIES;
    }
 
-   /**
-    * @return ProcessInstanceQuery
-    */
-   protected Query buildQuery()
-   {
-
-      Query query;
-
-      // Case search by ActivityInstanceQuery
-      if (caseOnlySearch && null != user)
-      {
-         query = getActivityQueryByProcessState(state);
-         FilterAndTerm filter = query.getFilter().addAndTerm();
-         if (null != oid)
-         {
-            filter.and(ActivityInstanceQuery.PROCESS_INSTANCE_OID.isEqual(oid));
-         }
-         filter.add(new PerformingUserFilter(user.getOID()));
-
-         filter.add(ActivityFilter.forProcess(PredefinedConstants.DEFAULT_CASE_ACTIVITY_ID,
-               PredefinedConstants.CASE_PROCESS_ID, false));
-
-         if (ProcessSearchProvider.ALL_PRIORITIES != priority)
-         {
-            filter.add(ActivityInstanceQuery.PROCESS_INSTANCE_PRIORITY.isEqual(priority));
-         }
-
-         if (null != startedFrom && null != startedTo)
-         {
-            filter.and(ActivityInstanceQuery.START_TIME.between(startedFrom.getTime(), startedTo.getTime()));
-         }
-         else if (null != startedTo)
-         {
-            filter.and(ActivityInstanceQuery.START_TIME.lessOrEqual(startedTo.getTime()));
-         }
-         else if (null != startedFrom)
-         {
-            filter.and(ActivityInstanceQuery.START_TIME.greaterOrEqual(startedFrom.getTime()));
-         }
-
-         return query;
-      }
-
-      // else create ProcessInstanceQuery
-
-      else if (state == ProcessActivityUtils.PROCESS_INSTANCE_STATE_ALIVE)
-      {
-         query = ProcessInstanceQuery.findInState(new ProcessInstanceState[] {
-               ProcessInstanceState.Active, ProcessInstanceState.Interrupted, ProcessInstanceState.Aborting});
-      }
-      else if (state == ProcessActivityUtils.PROCESS_INSTANCE_STATE_COMPLETED)
-      {
-         query = ProcessInstanceQuery.findInState(ProcessInstanceState.Completed);
-      }
-      else if (state == ProcessActivityUtils.PROCESS_INSTANCE_STATE_ABORTED)
-      {
-         query = ProcessInstanceQuery.findInState(ProcessInstanceState.Aborted);
-      }
-      else if (state == ProcessActivityUtils.PROCESS_INSTANCE_STATE_INTERRUPTED)
-      {
-         query = ProcessInstanceQuery.findInState(ProcessInstanceState.Interrupted);
-      }
-      else if (state == ProcessActivityUtils.PROCESS_INSTANCE_STATE_ABORTING)
-      {
-         query = ProcessInstanceQuery.findInState(ProcessInstanceState.Aborting);
-      }
-      else if (state == ProcessActivityUtils.PROCESS_INSTANCE_STATE_HALTING)
-      {
-         query = ProcessInstanceQuery.findInState(ProcessInstanceState.Halting);
-      }
-      else
-      {
-         query = ProcessInstanceQuery.findInState(new ProcessInstanceState[] {
-               ProcessInstanceState.Active, ProcessInstanceState.Completed, ProcessInstanceState.Interrupted,
-               ProcessInstanceState.Aborted, ProcessInstanceState.Aborting,  ProcessInstanceState.Aborting,  ProcessInstanceState.Halting});
-      }
-      FilterAndTerm filter = query.getFilter().addAndTerm();
-
-      if (null != startedFrom && null != startedTo)
-      {
-         filter.and(ProcessInstanceQuery.START_TIME.between(startedFrom.getTime(), startedTo.getTime()));
-      }
-      else if (startedTo != null)
-      {
-         filter.and(ProcessInstanceQuery.START_TIME.lessOrEqual(startedTo.getTime()));
-      }
-      else if (startedFrom != null)
-      {
-         filter.and(ProcessInstanceQuery.START_TIME.greaterOrEqual(startedFrom.getTime()));
-      }
-
-      if (null != endTimeFrom && null != endTimeTo)
-      {
-         filter.and(ProcessInstanceQuery.TERMINATION_TIME.between(endTimeFrom.getTime(), endTimeTo.getTime()));
-      }
-      else if (endTimeTo != null)
-      {
-         filter.and(ProcessInstanceQuery.TERMINATION_TIME.notEqual(0));
-         filter.and(ProcessInstanceQuery.TERMINATION_TIME.lessOrEqual(endTimeTo.getTime()));
-      }
-      else if (endTimeFrom != null)
-      {
-         filter.and(ProcessInstanceQuery.TERMINATION_TIME.greaterOrEqual(endTimeFrom.getTime()));
-      }
-
-      if (oid != null)
-      {
-         filter.and(ProcessInstanceQuery.OID.isEqual(oid.longValue()));
-      }
-      if (rootOid != null)
-      {
-         filter.and(ProcessInstanceQuery.ROOT_PROCESS_INSTANCE_OID.isEqual(rootOid.longValue()));
-      }
-      if (ProcessActivityUtils.ALL_PRIORITIES != priority)
-      {
-         filter.and(ProcessInstanceQuery.PRIORITY.isEqual(priority));
-      }
-
-      if (!includeCaseSearch)
-      {
-         FilterTerm orFilter = filter.addOrTerm();
-         ProcessDefinition caseProcessDefination = ModelCache.findModelCache().getCaseProcessDefination();
-         orFilter
-               .add(ProcessInstanceQuery.PROCESS_DEFINITION_OID.notEqual(caseProcessDefination.getRuntimeElementOID()));
-      }
-
-      if (includeRootProcess)
-      {
-         filter.and(ProcessInstanceHierarchyFilter.ROOT_PROCESS);
-      }
-      if (caseOnlySearch)
-      {
-         String qualifiedGroupId = "{" + PredefinedConstants.PREDEFINED_MODEL_ID + "}"
-               + PredefinedConstants.CASE_PROCESS_ID;
-         filter.add(new ProcessDefinitionFilter(qualifiedGroupId, false));
-      }
-      return query;
-   }
-
+  
    public Long getOid()
    {
       return oid;
@@ -335,33 +182,4 @@ public class FilterAttributesDTO implements Serializable
       return includeCaseSearch;
    }
 
-   /**
-    * 
-    * @param state
-    * @return
-    */
-   private ActivityInstanceQuery getActivityQueryByProcessState(int state)
-   {
-      ActivityInstanceQuery query = null;
-      switch (state)
-      {
-      case ProcessActivityUtils.PROCESS_INSTANCE_STATE_ALIVE:
-         query = ActivityInstanceQuery.findAlive();
-         break;
-      case ProcessActivityUtils.PROCESS_INSTANCE_STATE_COMPLETED:
-         query = ActivityInstanceQuery.findInState(ActivityInstanceState.Completed);
-         break;
-      case ProcessActivityUtils.PROCESS_INSTANCE_STATE_ABORTED:
-         query = ActivityInstanceQuery.findInState(ActivityInstanceState.Aborted);
-         break;
-      case ProcessActivityUtils.PROCESS_INSTANCE_STATE_INTERRUPTED:
-         query = ActivityInstanceQuery.findInState(ActivityInstanceState.Interrupted);
-         break;
-      default:
-         query = ActivityInstanceQuery.findAll();
-         break;
-
-      }
-      return query;
-   }
 }
